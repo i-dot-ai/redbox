bbc9b3ab70a3a2d6dde2e03036073fcc49a778fd
elliot.moore <elliotmoorecornwall@outlook.com>
2024-02-05 15:12:10 +0000

    Added default private to s3 bucket

--- infra/aws/terraform/modules/s3/main.tf
+++ infra/aws/terraform/modules/s3/main.tf
@@ -1,10 +1,9 @@
-// s3_module/main.tf
 variable "bucket_name" {}
 variable "lambda_function_arn" {
   default = ""
 }
 
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
   dynamic "notification_configuration" {
@@ -15,5 +14,19 @@ resource "aws_s3_bucket" "bucket" {
         events              = ["s3:ObjectCreated:*"]
       }
     }
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
