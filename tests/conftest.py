4fbae7ce4cce3e6ace41e6e2737288d2be8692d6
George Burton <g.e.c.cburton@gmail.com>
2024-02-15 17:20:01 +0000

    added test_list_items

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,23 +7,29 @@ from redbox.storage import FileSystemStorageHandler
 
 
 @pytest.fixture
-def uuid_str() -> str:
-    yield str(uuid4())
-
-
-@pytest.fixture
-def example_chuck(uuid_str) -> Chunk:
-    yield Chunk(parent_file_uuid=uuid_str, index=1, text="hello", metadata={})
-
-
-
-
-@pytest.fixture
 def file_system_storage_handler(tmp_path) -> FileSystemStorageHandler:
     yield FileSystemStorageHandler(tmp_path)
 
 
 @pytest.fixture
-def saved_example_chuck(example_chuck, file_system_storage_handler) -> Chunk:
-    file_system_storage_handler.write_item(example_chuck)
-    yield example_chuck
+def chunk() -> Chunk:
+    chunk = Chunk(
+        parent_file_uuid="test_uuid",
+        index=1,
+        text="test_text",
+        metadata={},
+        creator_user_uuid="test",
+    )
+    yield chunk
+
+
+@pytest.fixture
+def example_chuck_saved(chunk, file_system_storage_handler) -> Chunk:
+    file_system_storage_handler.write_item(chunk)
+    yield chunk
+
+
+@pytest.fixture
+def example_chuck_unsaved(file_system_storage_handler) -> Chunk:
+    chunk = Chunk(parent_file_uuid=str(uuid4()), index=1, text="hello", metadata={})
+    yield chunk
--- tests/redbox/storage/test_filesystem.py
+++ tests/redbox/storage/test_filesystem.py
@@ -1,15 +1,27 @@
 from uuid import uuid4
 
 
-def test_read_item(saved_example_chuck, file_system_storage_handler):
+def test_read_item(example_chuck_saved, file_system_storage_handler):
     obj = file_system_storage_handler.read_item(
-        saved_example_chuck.uuid, saved_example_chuck.__class__.__name__
+        example_chuck_saved.uuid, example_chuck_saved.__class__.__name__
     )
-    assert obj == saved_example_chuck
+    assert obj == example_chuck_saved
 
 
-def test_read_items(saved_example_chuck, file_system_storage_handler):
+def test_read_items(
+    example_chuck_saved, example_chuck_unsaved, file_system_storage_handler
+):
     objs = file_system_storage_handler.read_items(
-        [saved_example_chuck.uuid, str(uuid4())], saved_example_chuck.__class__.__name__
+        [example_chuck_saved.uuid, example_chuck_unsaved.uuid],
+        example_chuck_saved.__class__.__name__,
     )
-    assert objs == [saved_example_chuck]
+    assert objs == [example_chuck_saved]
+
+
+def test_list_all_items(
+    example_chuck_saved, example_chuck_unsaved, file_system_storage_handler
+):
+    objs = file_system_storage_handler.list_all_items(
+        example_chuck_saved.__class__.__name__
+    )
+    assert objs == [example_chuck_saved.uuid]
