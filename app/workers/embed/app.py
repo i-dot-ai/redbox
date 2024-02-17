5fc04dc7d7f5a039c4c2df35e6f4462763575ad8
Liam Wilkinson <liam.wilkinson@cabinetoffice.gov.uk>
2024-02-17 00:04:19 +0000

    Add queue listener for embedding requests

--- app/workers/embed/app.py
+++ app/workers/embed/app.py
@@ -1,10 +1,14 @@
+import json
 import logging
 import os
+import threading
+import time
 from contextlib import asynccontextmanager
 from datetime import datetime
 from typing import List
 from uuid import uuid4
 
+import pika
 import pydantic
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
@@ -47,6 +51,11 @@ class StatusResponse(pydantic.BaseModel):
     version: str
 
 
+class EmbedQueueItem(pydantic.BaseModel):
+    model: str
+    sentence: str
+
+
 # === API Setup ===
 
 
@@ -55,8 +64,8 @@ IS_READY = False
 available_models = []
 models = {}
 model_info = {}
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log = logging.getLogger()
+log.setLevel(logging.INFO)
 
 
 @asynccontextmanager
@@ -85,9 +94,23 @@ async def lifespan(app: FastAPI):
         model_info[model] = model_info_entry
 
     IS_READY = True
+
+    # Check to see if we run in polling mode from a queue
+    if "QUEUE_URI" in os.environ:
+        log.info("Queue URI found, starting queue listener.")
+        queue_uri = os.environ["QUEUE_URI"]
+        queue_name = os.environ.get("EMBED_QUEUE_NAME", "redbox-embed-queue")
+        queue_poll_interval = int(os.environ.get("QUEUE_POLL_INTERVAL", 5))
+
+        poll_thread = threading.Thread(
+            target=poll_queue_every, args=(queue_uri, queue_name, queue_poll_interval)
+        )
+        poll_thread.start()
+
     # End of the setup phase, yield control back to FastAPI
     yield
     # FastAPI is shutting down, perform cleanup...
+    poll_thread.join()
     log.info("Received shutdown event in the lifespan context, cleaning up.")
     IS_READY = False
     models.clear()
@@ -210,3 +233,70 @@ def embed_sentences(model: str, sentences: List[str]):
     }
 
     return output
+
+
+def poll_queue_every(queue_uri: str, queue_name: str, interval: int = 5):
+    logging.debug(f"Starting queue poller for {queue_name} every {interval} seconds")
+    while True:
+        logging.debug(f"Polling queue {queue_name} (every {interval} seconds)")
+        poll_queue(queue_uri, queue_name)
+        time.sleep(interval)
+
+
+def poll_queue(queue_uri: str, queue_name: str, max_connection_attempts: int = 10):
+    logging.info(f"Polling queue {queue_name}")
+
+    connection = None
+
+    for i in range(max_connection_attempts):
+        try:
+            logging.debug(
+                f"Attempting to connect to queue {queue_name} (attempt {i}/{max_connection_attempts})"
+            )
+            connection = pika.BlockingConnection(
+                parameters=pika.URLParameters(queue_uri),
+            )
+            logging.debug(f"Connected to queue {queue_name}")
+            break
+        except Exception as e:
+            logging.error(f"Failed to connect to queue attempt {i}: {e}")
+            time.sleep(5)
+
+    if not connection:
+        logging.error("Failed to connect to queue, shutting down")
+        return
+
+    channel = connection.channel()
+    channel.queue_declare(queue=queue_name, durable=True)
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=embed_item_callback, auto_ack=False
+    )
+    channel.start_consuming()
+
+
+def embed_item_callback(ch, method, properties, body):
+    logging.info(f"Received message {method.delivery_tag} by callback")
+
+    try:
+        body_dict = json.loads(body.decode("utf-8"))
+    except json.JSONDecodeError as e:
+        logging.error(f"Failed to decode message: {e}")
+        ch.basic_nack(delivery_tag=method.delivery_tag)
+        return
+    try:
+        embed_queue_item = EmbedQueueItem(**body_dict)
+    except pydantic.ValidationError as e:
+        logging.error(f"Failed to validate message: {e}")
+        ch.basic_nack(delivery_tag=method.delivery_tag)
+        return
+
+    output = embed_sentences(embed_queue_item.model, [embed_queue_item.sentence])
+    output = EmbeddingResponse(**output)
+
+    # TODO: Send the output to Elasticsearch?
+
+    logging.info(
+        f"Embedding ID {output.embedding_id} complete for {method.delivery_tag}"
+    )
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    ch.stop_consuming()
