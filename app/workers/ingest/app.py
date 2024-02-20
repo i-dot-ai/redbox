b5fdbeaa8580e7393f994c6026f18fbbf65bac8a
Liam Wilkinson <liam.wilkinson@cabinetoffice.gov.uk>
2024-02-20 22:20:31 +0000

    Ingest worker

--- app/workers/ingest/app.py
+++ app/workers/ingest/app.py
@@ -1,64 +1,140 @@
-from datetime import datetime
+import json
+import logging
+import os
 
-import pydantic
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+import boto3
+import pika
+from elasticsearch import Elasticsearch
 
-# === Data Models ===
+from redbox.models import File
+from redbox.parsing.file_chunker import FileChunker
+from redbox.storage.elasticsearch import ElasticsearchStorageHandler
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+ENV = {
+    "ELASTIC_USER": None,
+    "ELASTIC_PASSWORD": None,
+    "ELASTIC_HOST": None,
+    "ELASTIC_PORT": None,
+    "ELASTIC_SCHEME": None,
+    "OBJECT_STORE": None,
+    "BUCKET_NAME": None,
+    "INGEST_QUEUE_NAME": None,
+    "QUEUE": None,
+}
+
+for key in ENV:
+    # Throw KeyError if the environment variable is not set
+    ENV[key] = os.environ[key]
 
 
-class StatusResponse(pydantic.BaseModel):
-    status: str
-    uptime_seconds: float
-    version: str
+# === Object Store ===
+
+if ENV["OBJECT_STORE"] == "minio":
+    for key in ["MINIO_HOST", "MINIO_PORT", "MINIO_ACCESS_KEY", "MINIO_SECRET_KEY"]:
+        ENV[key] = os.environ[key]
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=ENV["MINIO_ACCESS_KEY"],
+        aws_secret_access_key=ENV["MINIO_SECRET_KEY"],
+        endpoint_url=f"http://{ENV['MINIO_HOST']}:{ENV['MINIO_PORT']}",
+    )
+elif ENV["OBJECT_STORE"] == "s3":
+    for key in ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_REGION"]:
+        ENV[key] = os.environ[key]
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"],
+        region_name=ENV["AWS_REGION"],
+    )
 
 
-# === API Setup ===
+# === Queues ===
 
-start_time = datetime.now()
-IS_READY = False
+if ENV["QUEUE"] == "rabbitmq":
+    for key in ["RABBITMQ_HOST", "RABBITMQ_PORT", "RABBITMQ_USER", "RABBITMQ_PASSWORD"]:
+        ENV[key] = os.environ[key]
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=ENV["RABBITMQ_HOST"],
+            port=int(ENV["RABBITMQ_PORT"]),
+            credentials=pika.PlainCredentials(
+                ENV["RABBITMQ_USER"], ENV["RABBITMQ_PASSWORD"]
+            ),
+            connection_attempts=5,
+            retry_delay=5,
+        )
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue=ENV["INGEST_QUEUE_NAME"], durable=True)
+elif ENV["QUEUE"] == "sqs":
+    for key in ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_REGION"]:
+        ENV[key] = os.environ[key]
+
+    sqs = boto3.client(
+        "sqs",
+        aws_access_key_id=ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"],
+        region_name=ENV["AWS_REGION"],
+    )
+
+    raise NotImplementedError("SQS is not yet implemented")
+
+# === Storage ===
 
 
-# Create API
-
-app = FastAPI(
-    title="Ingest API",
-    description="An API for ingesting data into the system",
-    version="0.1.0",
-    openapi_tags=[
-        {"name": "health", "description": "Health check"},
+es = Elasticsearch(
+    hosts=[
+        {
+            "host": ENV["ELASTIC_HOST"],
+            "port": int(ENV["ELASTIC_PORT"]),
+            "scheme": ENV["ELASTIC_SCHEME"],
+        }
     ],
-    docs_url="/docs",
-    redoc_url="/redoc",
-    openapi_url="/openapi.json",
+    basic_auth=(ENV["ELASTIC_USER"], ENV["ELASTIC_PASSWORD"]),
 )
 
-# === API Routes ===
-
-# Basic Setup
-
-
-@app.get("/", include_in_schema=False, response_class=RedirectResponse)
-def root():
-    return RedirectResponse(url="/docs")
+storage_handler = ElasticsearchStorageHandler(es_client=es, root_index="redbox-data")
+chunker = FileChunker()
 
 
-@app.get("/health", response_model=StatusResponse, tags=["health"])
-def health():
-    """Returns the health of the API
+def ingest_file(file: File):
+    logging.info(f"Ingesting file: {file}")
 
-    Returns:
-        StatusResponse: The health of the API
-    """
+    authenticated_s3_url = s3.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": ENV["BUCKET_NAME"], "Key": file.name},
+        ExpiresIn=180,
+    )
 
-    uptime = datetime.now() - start_time
-    uptime_seconds = uptime.total_seconds()
+    chunks = chunker.chunk_file(
+        file=file,
+        file_url=authenticated_s3_url,
+        creator_user_uuid=file.creator_user_uuid,
+    )
 
-    output = {"status": None, "uptime_seconds": uptime_seconds, "version": app.version}
+    # remove URL from metadata added by unstructured chunker
+    for chunk in chunks:
+        del chunk["metadata"]["url"]
 
-    if IS_READY:
-        output["status"] = "ready"
-    else:
-        output["status"] = "loading"
+    logging.info(f"Writing {len(chunks)} chunks to storage for file uuid: {file.uuid}")
 
-    return output
+    storage_handler.write_items(chunks)
+
+
+def callback(ch, method, properties, body):
+    logging.info("Received message")
+    file = File(**json.loads(body))
+    logging.info(f"Starting ingest for file (uuid: {file.uuid}, name: {file.name})")
+    ingest_file(file)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_consume(queue=ENV["INGEST_QUEUE_NAME"], on_message_callback=callback)
+channel.start_consuming()
