41a359c86f9c69b6fc9ee98e512baff5ede161c4
George Burton <g.e.c.cburton@gmail.com>
2024-02-26 21:07:20 +0000

    added comments to tests

--- tests/app/workers/embed/test_app.py
+++ tests/app/workers/embed/test_app.py
@@ -1,4 +1,9 @@
 def test_read_all_models(client, example_modes):
+    """
+    Given that I have one model, paraphrase-albert-small-v2, in the database
+    When I GET all models /models
+    I Expect a list of just one model, paraphrase-albert-small-v2, to be returned
+    """
     response = client.get("/models")
     assert response.status_code == 200
     assert response.json() == {
@@ -13,6 +18,11 @@ def test_read_all_models(client, example_modes):
 
 
 def test_read_one_model(client, example_modes):
+    """
+    Given that I have one model, paraphrase-albert-small-v2, in the database
+    When I GET this one model /models/paraphrase-albert-small-v2
+    I Expect a single model, paraphrase-albert-small-v2, to be returned
+    """
     response = client.get("/models/paraphrase-albert-small-v2")
     assert response.status_code == 200
     assert response.json() == {
@@ -23,12 +33,22 @@ def test_read_one_model(client, example_modes):
 
 
 def test_read_models_404(client, example_modes):
+    """
+    Given that I have one model, paraphrase-albert-small-v2, in the database
+    When I GET a non-existent model /models/not-a-model
+    I Expect a 404 error
+    """
     response = client.get("/models/not-a-model")
     assert response.status_code == 404
     assert response.json() == {"detail": "Model not-a-model not found"}
 
 
 def test_embed_sentences_422(client):
+    """
+    Given that I have one model, paraphrase-albert-small-v2, in the database
+    When I POST a mall-formed payload to /models/paraphrase-albert-small-v2/embed
+    I Expect a 422 error
+    """
     response = client.post(
         "/models/paraphrase-albert-small-v2/embed",
         json={"not": "a well formed payload"},
@@ -38,6 +58,14 @@ def test_embed_sentences_422(client):
 
 
 def test_embed_sentences(client):
+    """
+    Given that I have one model, paraphrase-albert-small-v2, in the database
+    When I POST a valid payload consisting of some sentenced to embed to
+    /models/paraphrase-albert-small-v2/embed
+    I Expect a 200 response
+
+    N.B. We are not testing the content / efficacy of the model in this test.
+    """
     response = client.post(
         "/models/paraphrase-albert-small-v2/embed",
         json=["I am the egg man", "I am the walrus"],
