46864ca64811634fba0fd3a8a9f436dc271cfeaa
George Burton <g.e.c.cburton@gmail.com>
2024-03-01 11:22:53 +0000

    run mypy on embed and ingest

--- embed/tests/conftest.py
+++ embed/tests/conftest.py
@@ -1,15 +1,8 @@
-import os
-
 import pytest
 from sentence_transformers import SentenceTransformer
 
 from fastapi.testclient import TestClient
 from embed.src.app import app as application, model_db
-
-env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env.test")
-
-
-# env = Settings(_env_file=env_path)  # type: ignore
 
 
 def file_pdf_path() -> str:
