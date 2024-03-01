d6b26db1dc550005098e919227df1d3ec2aac705
elliot.moore <elliotmoorecornwall@outlook.com>
2024-03-01 14:06:31 +0000

    Removed TOTP requirement from admin site

--- django_app/redbox_app/redbox_core/admin.py
+++ django_app/redbox_app/redbox_core/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
-from django_otp.admin import OTPAdminSite
 
 from . import models
-
-
-class OTPAdmin(OTPAdminSite):
-    pass
-
-
-admin_site = OTPAdmin(name="OTPAdmin")
 
 
 admin.site.register(models.User)
