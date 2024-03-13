e7097512e53ea68c268da5a038bcb57eb3d299cd
George Burton <g.e.c.cburton@gmail.com>
2024-03-13 13:19:07 +0000

    now testing side effects

--- core_api/tests/conftest.py
+++ core_api/tests/conftest.py
@@ -14,9 +14,6 @@ T = TypeVar("T")
 YieldFixture = Generator[T, None, None]
 
 
-assert env.elastic_port == 9200
-
-
 @pytest.fixture
 def s3_client():
     yield env.s3_client()
@@ -28,7 +25,7 @@ def es_client() -> YieldFixture[Elasticsearch]:
 
 
 @pytest.fixture
-def client():
+def app_client():
     yield TestClient(application)
 
 
@@ -38,13 +35,45 @@ def elasticsearch_storage_handler(es_client):
 
 
 @pytest.fixture
-def file():
-    yield File(name="my-file", path="users/my-file.txt", type="txt")
+def file(s3_client, file_pdf_path, bucket) -> YieldFixture[File]:
+    """
+    TODO: this is a cut and paste of core_api:create_upload_file
+    When we come to test core_api we should think about
+    the relationship between core_api and the ingest app
+    """
+    file_name = os.path.basename(file_pdf_path)
+    file_type = file_name.split(".")[-1]
+
+    with open(file_pdf_path, "rb") as f:
+        s3_client.put_object(
+            Bucket=bucket,
+            Body=f.read(),
+            Key=file_name,
+            Tagging=f"file_type={file_type}",
+        )
+
+    authenticated_s3_url = s3_client.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": env.bucket_name, "Key": file_name},
+        ExpiresIn=3600,
+    )
+
+    # Strip off the query string (we don't need the keys)
+    simple_s3_url = authenticated_s3_url.split("?")[0]
+    file_record = File(
+        name=file_name,
+        path=simple_s3_url,
+        type=file_type,
+        creator_user_uuid="dev",
+        storage_kind=env.object_store,
+    )
+
+    yield file_record
 
 
 @pytest.fixture
-def stored_file(elasticsearch_storage_handler, file):
-    elasticsearch_storage_handler.write_item(item=file)
+def stored_file(elasticsearch_storage_handler, file) -> YieldFixture[File]:
+    elasticsearch_storage_handler.write_item(file)
     yield file
 
 
--- core_api/tests/test_app.py
+++ core_api/tests/test_app.py
@@ -1,24 +1,45 @@
-def test_get_health(client):
-    response = client.get("/health")
+import pytest
+from elasticsearch import NotFoundError
+
+
+def test_get_health(app_client):
+    response = app_client.get("/health")
     assert response.status_code == 200
 
 
-def test_post_file_upload(client, bucket, file_pdf_path, file):
+def test_post_file_upload(s3_client, app_client, elasticsearch_storage_handler, bucket, file_pdf_path):
     with open(file_pdf_path, "rb") as f:
-        response = client.post("/file", files={"file": ("filename", f, "pdf")})
+        response = app_client.post("/file", files={"file": ("filename", f, "pdf")})
+    assert response.status_code == 200
+    assert s3_client.get_object(Bucket=bucket, Key=file_pdf_path.split("/")[-1])
+    json_response = response.json()
+    assert elasticsearch_storage_handler.read_item(
+        item_uuid=json_response["uuid"], model_type=json_response["model_type"]
+    )
+
+
+def test_get_file(app_client, stored_file):
+    response = app_client.get(f"/file/{stored_file.uuid}")
     assert response.status_code == 200
 
 
-def test_get_file(client, stored_file):
-    response = client.get(f"/file/{stored_file.uuid}")
+def test_delete_file(s3_client, app_client, elasticsearch_storage_handler, bucket, stored_file):
+    # check assets exist
+    assert s3_client.get_object(Bucket=bucket, Key=stored_file.name)
+    assert elasticsearch_storage_handler.read_item(item_uuid=stored_file.uuid, model_type="file")
+
+    response = app_client.delete(f"/file/{stored_file.uuid}")
     assert response.status_code == 200
 
+    # check assets dont exist
+    with pytest.raises(Exception):
+        s3_client.get_object(Bucket=bucket, Key=stored_file.name)
 
-def test_delete_file(client, stored_file):
-    response = client.delete(f"/file/{stored_file.uuid}")
+    with pytest.raises(NotFoundError):
+        elasticsearch_storage_handler.read_item(item_uuid=stored_file.uuid, model_type="file")
+
+
+def test_ingest_file(app_client, stored_file):
+    response = app_client.post(f"/file/{stored_file.uuid}/ingest/")
     assert response.status_code == 200
-
-
-def test_ingest_file(client, stored_file):
-    response = client.post(f"/file/{stored_file.uuid}/ingest/")
-    assert response.status_code == 200
+    # TODO: check that message is in queue
