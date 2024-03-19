b3df1eb90dea1cc96573bcd6fed3c954b16494ff
elliot.moore <elliotmoorecornwall@outlook.com>
2024-03-19 14:50:13 +0000

    Try get sentence transformer from core instead

--- embed/tests/conftest.py
+++ embed/tests/conftest.py
@@ -5,7 +5,8 @@ import pytest
 from elasticsearch import Elasticsearch
 from sentence_transformers import SentenceTransformer
 
-from model_db import SentenceTransformerDB
+from core_api.src.app import model_db
+# from model_db import SentenceTransformerDB
 from redbox.models import Chunk, EmbedQueueItem, Settings
 from redbox.storage import ElasticsearchStorageHandler
 
@@ -13,8 +14,8 @@ T = TypeVar("T")
 
 YieldFixture = Generator[T, None, None]
 
-model_db = SentenceTransformerDB()
-model_db.init_from_disk()
+# model_db = SentenceTransformerDB()
+# model_db.init_from_disk()
 
 
 env = Settings()
