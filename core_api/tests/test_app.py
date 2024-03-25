a4f183249226f14dccf4e1682a290613a2f8dcaa
George Burton <g.e.c.cburton@gmail.com>
2024-03-25 13:26:00 +0000

    fix core-api unit tests

--- core_api/tests/test_app.py
+++ core_api/tests/test_app.py
@@ -1,7 +1,9 @@
 import pytest
 from elasticsearch import NotFoundError
 
-from core_api.src.app import env, publisher
+from faststream.rabbit import TestRabbitBroker
+
+from core_api.src.app import env, publisher, router
 from redbox.models import ProcessingStatusEnum
 
 
@@ -15,14 +17,16 @@ def test_get_health(app_client):
     assert response.status_code == 200
 
 
-def test_post_file_upload(s3_client, app_client, elasticsearch_storage_handler, bucket, file_pdf_path):
+@pytest.mark.asyncio
+async def test_post_file_upload(s3_client, app_client, elasticsearch_storage_handler, bucket, file_pdf_path):
     """
     Given a new file
     When I POST it to /file
     I Expect to see it persisted in s3 and elastic-search
     """
     with open(file_pdf_path, "rb") as f:
-        response = app_client.post("/file", files={"file": ("filename", f, "pdf")})
+        async with TestRabbitBroker(router.broker):
+            response = app_client.post("/file", files={"file": ("filename", f, "pdf")})
     assert response.status_code == 200
     assert s3_client.get_object(Bucket=bucket, Key=file_pdf_path.split("/")[-1])
     json_response = response.json()
@@ -74,18 +78,19 @@ async def test_ingest_file(app_client, stored_file, elasticsearch_storage_handle
     When I POST to /file/uuid/ingest
     I Expect to see a message on the ingest-queue, THIS IS NOT WORKING
     """
-    response = app_client.post(f"/file/{stored_file.uuid}/ingest/")
+    async with TestRabbitBroker(router.broker):
+        response = app_client.post(f"/file/{stored_file.uuid}/ingest/")
 
-    assert (
-        elasticsearch_storage_handler.read_item(
-            item_uuid=stored_file.uuid,
-            model_type="file",
-        ).processing_status
-        is ProcessingStatusEnum.parsing
-    )
-    assert response.status_code == 200
+        assert (
+            elasticsearch_storage_handler.read_item(
+                item_uuid=stored_file.uuid,
+                model_type="file",
+            ).processing_status
+            is ProcessingStatusEnum.parsing
+        )
+        assert response.status_code == 200
 
-    publisher.mock.called_once_with(stored_file)
+        publisher.mock.called_once_with(stored_file)
 
 
 def test_read_all_models(client):
