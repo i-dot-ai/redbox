ab8bdf0234cbced23f0f627c7ad49261953191e3
George Burton <g.e.c.cburton@gmail.com>
2024-03-28 10:10:57 +0000

    restored test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -13,19 +13,18 @@ def test_upload_to_elastic(file_pdf_path):
 
     file_name = os.path.basename(file_pdf_path)
     files = {"file": (file_name, open(file_pdf_path, "rb"), "application/pdf")}
-    _response = requests.post(url="http://localhost:5002/file", files=files)
-    time.sleep(60)
-    # assert response.status_code == 200
-    # file_uuid = response.json()["uuid"]
-    #
-    # timeout = 60  # 10s should be plenty
-    # start_time = time.time()
-    # embeddings_found = False
-    #
-    # while not embeddings_found and time.time() - start_time < timeout:
-    #     time.sleep(1)
-    #     chunks = requests.get(f"http://localhost:5002/file/{file_uuid}/chunks").json()
-    #     embeddings_found = any(chunk["embedding"] for chunk in chunks)
-    #
-    # if not embeddings_found:
-    #     pytest.fail(reason=f"failed to get embedded chunks within {timeout} seconds")
+    response = requests.post(url="http://localhost:5002/file", files=files)
+    assert response.status_code == 200
+    file_uuid = response.json()["uuid"]
+
+    timeout = 60  # 10s should be plenty
+    start_time = time.time()
+    embeddings_found = False
+
+    while not embeddings_found and time.time() - start_time < timeout:
+        time.sleep(1)
+        chunks = requests.get(f"http://localhost:5002/file/{file_uuid}/chunks").json()
+        embeddings_found = any(chunk["embedding"] for chunk in chunks)
+
+    if not embeddings_found:
+        pytest.fail(reason=f"failed to get embedded chunks within {timeout} seconds")
