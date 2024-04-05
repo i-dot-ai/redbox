9e5c04faab4d8b19c35e6583e04a2f87793a291e
George Burton <g.e.c.cburton@gmail.com>
2024-04-05 14:57:48 +0100

    corrected file path

--- django_app/tests/conftest.py
+++ django_app/tests/conftest.py
@@ -60,6 +60,9 @@ def mrs_tiggywinkle():
 def file_pdf_path():
     path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
+        "..",
+        "..",
+        "tests",
         "data",
         "pdf",
         "Cabinet Office - Wikipedia.pdf",
