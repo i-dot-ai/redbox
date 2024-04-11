90b97b7fd95fc846bc53d175bb70f8d8662d000e
George Burton <g.e.c.cburton@gmail.com>
2024-04-11 14:27:03 +0100

    now creating bucket

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import os
 
 import boto3
 import pytest
+from botocore.exceptions import ClientError
 
 
 @pytest.fixture
@@ -17,9 +18,22 @@ def file_pdf_path():
 
 @pytest.fixture
 def s3_client():
-    yield boto3.client(
+    client = boto3.client(
         "s3",
         aws_access_key_id="minioadmin",
         aws_secret_access_key="minioadmin",
-        endpoint_url=f"http://localhost:9000",
+        endpoint_url="http://localhost:9000",
     )
+
+    try:
+        client.create_bucket(
+            Bucket="redbox-storage-dev",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] != "BucketAlreadyOwnedByYou":
+            raise e
+
+    yield client
+
+
