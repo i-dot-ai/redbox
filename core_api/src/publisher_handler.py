8896f465c83b12f7dc4ea0c1ef46fa987c1df58b
George Burton <g.e.c.cburton@gmail.com>
2024-04-13 10:32:52 +0100

    added publisher-handler

---
+++ core_api/src/publisher_handler.py
@@ -0,0 +1,23 @@
+from faststream.redis import RedisBroker
+
+from redbox.models import File
+
+
+class FilePublisher:
+    """This class is a bit of a hack to overcome a shortcoming (bug?) in faststream
+    whereby the broker is not automatically connected in sub-applications.
+
+    TODO: fix this properly, or raise an issue against faststream
+    """
+
+    def __init__(self, broker: RedisBroker, queue_name: str):
+        self.connected = False
+        self.broker = broker
+        self.queue_name = queue_name
+
+    async def publish(self, file: File):
+        if not self.connected:
+            # we only do this once
+            await self.broker.connect()
+            self.connected = True
+        return self.broker.publish(file, self.queue_name)
