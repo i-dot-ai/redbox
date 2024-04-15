366b7341268d6e13d073d9a56b7f24e363be267c
George Burton <g.e.c.cburton@gmail.com>
2024-04-15 12:19:17 +0100

    added a redis insatance

--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -3,7 +3,29 @@ variable "account_id" {
   description = "AWS Account ID"
 }
 
+variable "region" {
+  type = string
+  default = "eu-west-2"
+  description = "AWS region"
+}
+
+variable "state_bucket" {
+  type        = string
+  description = "Name of the S3 bucket to use a terraform state"
+}
+
+variable "image_tag" {
+  type        = string
+  description = "Image tag"
+}
+
 variable "project_name" {
   type        = string
-  description = "Name of project"
+  description = "Name of the project"
 }
+
+variable "env" {
+  type        = string
+  description = "Environment"
+}
+
