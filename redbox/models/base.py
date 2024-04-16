4bfd8f389ceda70bdc10bdd245fcebc262dfe029
Liam Wilkinson <liam.wilkinson@cabinetoffice.gov.uk>
2024-04-16 16:55:42 +0100

    Ruff

--- redbox/models/base.py
+++ redbox/models/base.py
@@ -8,13 +8,9 @@ from pydantic import AfterValidator, BaseModel, Field, computed_field
 class PersistableModel(BaseModel):
     """Base class for all models that can be persisted to the database."""
 
-    uuid: UUID | Annotated[str, AfterValidator(lambda x: UUID(x))] = Field(
-        default_factory=uuid4
-    )
+    uuid: UUID | Annotated[str, AfterValidator(lambda x: UUID(x))] = Field(default_factory=uuid4)
     created_datetime: datetime = Field(default_factory=datetime.utcnow)
-    creator_user_uuid: Optional[
-        UUID | Annotated[str, AfterValidator(lambda x: UUID(x))]
-    ] = None
+    creator_user_uuid: Optional[UUID | Annotated[str, AfterValidator(lambda x: UUID(x))]] = None
 
     @computed_field
     def model_type(self) -> str:
--- redbox/models/file.py
+++ redbox/models/file.py
@@ -28,23 +28,15 @@ class File(PersistableModel):
 class Chunk(PersistableModel):
     """Chunk of a File"""
 
-    parent_file_uuid: UUID = Field(
-        description="id of the original file which this text came from"
-    )
-    index: int = Field(
-        description="relative position of this chunk in the original file"
-    )
+    parent_file_uuid: UUID = Field(description="id of the original file which this text came from")
+    index: int = Field(description="relative position of this chunk in the original file")
     text: str = Field(description="chunk of the original text")
     metadata: dict
-    embedding: Optional[list[float]] = Field(
-        description="the vector representation of the text", default=None
-    )
+    embedding: Optional[list[float]] = Field(description="the vector representation of the text", default=None)
 
     @computed_field
     def text_hash(self) -> str:
-        return hashlib.md5(
-            self.text.encode(encoding="UTF-8", errors="strict"), usedforsecurity=False
-        ).hexdigest()
+        return hashlib.md5(self.text.encode(encoding="UTF-8", errors="strict"), usedforsecurity=False).hexdigest()
 
     @computed_field
     def token_count(self) -> int:
@@ -68,8 +60,6 @@ class FileStatus(BaseModel):
 
 class FileExistsException(Exception):
     def __init__(self):
-        super().__init__(
-            "Document with same name already exists. Please rename if you want to upload anyway."
-        )
+        super().__init__("Document with same name already exists. Please rename if you want to upload anyway.")
 
     pass
