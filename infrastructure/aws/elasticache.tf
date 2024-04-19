6440fc5217fb00f47d931fbe1a00b3916c67e7c6
George Burton <g.e.c.cburton@gmail.com>
2024-04-19 16:41:11 +0100

    corrected sg

--- infrastructure/aws/elasticache.tf
+++ infrastructure/aws/elasticache.tf
@@ -5,5 +5,5 @@ module "elasticache" {
   prefix            = "backend"
   vpc_id            = data.terraform_remote_state.vpc.outputs.vpc_id
   private_subnets   = data.terraform_remote_state.vpc.outputs.private_subnets
-  security_group_id = module.load_balancer.load_balancer_security_group_id
+  security_group_id = module.core_api.ecs_sg_id
 }
