cbfc82b62a59e1597c44ef14a9bb32350db4bd79
George Burton <g.e.c.cburton@gmail.com>
2024-04-19 07:43:07 +0100

    added s3:createBucket

--- infrastructure/aws/iam.tf
+++ infrastructure/aws/iam.tf
@@ -12,6 +12,7 @@ data "aws_iam_policy_document" "ecs_exec_role_policy" {
       "s3:DeleteObject",
       "s3:GetObjectVersion",
       "s3:ListMultipartUploadParts",
+      "s3:createBucket"
     ]
     resources = [
       "arn:aws:s3:::${local.bucket_name}",
