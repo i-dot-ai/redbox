ebd95709ff27cb6e2f5413b90e1d712c8927de19
Rachael Robinson <23265724+rachaelcodes@users.noreply.github.com>
2024-04-22 10:34:45 +0100

    first pass at writing rag endpoint

--- core_api/src/routes/chat.py
+++ core_api/src/routes/chat.py
@@ -1,12 +1,21 @@
 from typing import List, Literal
+from uuid import UUID
 
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from langchain.chains.llm import LLMChain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain_community.chat_models import ChatLiteLLM
+from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_elasticsearch import ApproxRetrievalStrategy, ElasticsearchStore
 from typing_extensions import TypedDict
 
+from redbox.llm.prompts.chat import (
+    CONDENSE_QUESTION_PROMPT,
+    STUFF_DOCUMENT_PROMPT,
+    WITH_SOURCES_PROMPT,
+)
 from redbox.models import Settings
 
 env = Settings()
@@ -25,10 +34,29 @@ chat_app = FastAPI(
 )
 
 
+# === LLM setup ===
+
+
 llm = ChatLiteLLM(
     model="gpt-3.5-turbo",  # TODO: set with env var
     # TODO: set max_tokens and temperature
     streaming=True,
+)
+
+es = env.elasticsearch_client()
+embedding_function = SentenceTransformerEmbeddings()
+# TODO: do we want to be able to set hybrid to True depending on Elastic subscription levels?
+# see: https://github.com/i-dot-ai/redbox-copilot-streamlit/blob/3d197e76e6d42cfe0b70f66bb374c899b74754b4/streamlit_app/utils.py#L316C17-L316C45
+hybrid = False
+
+# TODO: fix this - I don't think it's correctly implemented, as we're getting an error when making a request:
+# raise HTTP_EXCEPTIONS.get(meta.status, ApiError)(
+# elasticsearch.NotFoundError: NotFoundError(404, 'status_exception', '[file] is not an inference service model or a deployed ml model')
+vector_store = ElasticsearchStore(
+    es_connection=es,
+    index_name="redbox-data",
+    embedding=embedding_function,
+    strategy=ApproxRetrievalStrategy(hybrid=hybrid, query_model_id="file"),
 )
 
 
@@ -84,3 +112,45 @@ def simple_chat(chat_history: List[ChatMessage]) -> StreamingResponse:
     chain = LLMChain(llm=llm, prompt=chat_prompt)
 
     return chain.stream({"input": question["text"]})
+
+
+@chat_app.post("/rag", tags=["chat"])
+def rag_chat(chat_history: List[ChatMessage], files: List[UUID]) -> StreamingResponse:
+    """Get a LLM response to a question history and file
+
+    Args:
+
+
+    Returns:
+        StreamingResponse: a stream of the chain response
+    """
+    question = chat_history[-1]
+    previous_history = chat_history[0:-1]
+
+    docs_with_sources_chain = load_qa_with_sources_chain(
+        llm,
+        chain_type="stuff",
+        prompt=WITH_SOURCES_PROMPT,
+        document_prompt=STUFF_DOCUMENT_PROMPT,
+        verbose=True,
+    )
+
+    condense_question_chain = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
+
+    # split chain manually, so that the standalone question doesn't leak into chat
+    standalone_question = condense_question_chain({"question": question, "chat_history": previous_history})["text"]
+
+    # TODO: limit this to the user provided documents
+    # also, this is currently causing a 404 error (see error in note line 52)
+    docs = vector_store.as_retriever().get_relevant_documents(standalone_question)
+
+    result = docs_with_sources_chain(
+        {
+            "question": standalone_question,
+            "input_documents": docs,
+            # TODO: do we want user_info or current_date
+        },
+        # TODO: do we need callbacks?
+    )
+
+    return result, docs_with_sources_chain
