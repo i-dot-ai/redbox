8b5ff50bdf0d476b432446f2b0345ea45a4530ac
elliot.moore <elliotmoorecornwall@outlook.com>
2024-04-24 09:52:35 +0100

    Added missing certificate_arn to load_balancer.tf

--- infrastructure/aws/load_balancer.tf
+++ infrastructure/aws/load_balancer.tf
@@ -1,11 +1,12 @@
 module "load_balancer" {
-  source         = "../../../i-ai-core-infrastructure/modules/load_balancer"
-  project        = var.project_name
-  region         = var.region
-  state_bucket   = var.state_bucket
-  account_id     = var.account_id
-  vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
-  public_subnets = data.terraform_remote_state.vpc.outputs.public_subnets
-  ip_whitelist   = concat(var.internal_ips, var.developer_ips, var.external_ips)
-  env            = var.env
+  source          = "../../../i-ai-core-infrastructure/modules/load_balancer"
+  project         = var.project_name
+  region          = var.region
+  state_bucket    = var.state_bucket
+  account_id      = var.account_id
+  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnets  = data.terraform_remote_state.vpc.outputs.public_subnets
+  ip_whitelist    = concat(var.internal_ips, var.developer_ips, var.external_ips)
+  env             = var.env
+  certificate_arn = data.terraform_remote_state.universal.outputs.certificate_arn
 }
