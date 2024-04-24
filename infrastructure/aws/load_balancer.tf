5b6f64e17cf1b4b8825e494583ffa679a74db1ea
Guy Hannay <ghannay@no10.gov.uk>
2024-04-24 09:03:41 +0100

    add: waf

--- infrastructure/aws/load_balancer.tf
+++ infrastructure/aws/load_balancer.tf
@@ -1,11 +1,50 @@
 module "load_balancer" {
-  source         = "../../../i-ai-core-infrastructure/modules/load_balancer"
+  source          = "../../../i-ai-core-infrastructure/modules/load_balancer"
+  project         = var.project_name
+  region          = var.region
+  state_bucket    = var.state_bucket
+  account_id      = var.account_id
+  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnets  = data.terraform_remote_state.vpc.outputs.public_subnets
+  ip_whitelist    = concat(var.internal_ips, var.developer_ips, var.external_ips)
+  env             = var.env
+  certificate_arn = data.terraform_remote_state.universal.outputs.certificate_arn
+  web_acl_arn     = module.waf.web_acl_arn
+}
+
+
+
+module "waf" {
+  source         = "../../../i-ai-core-infrastructure/modules/waf"
   project        = var.project_name
-  region         = var.region
-  state_bucket   = var.state_bucket
-  account_id     = var.account_id
-  vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
-  public_subnets = data.terraform_remote_state.vpc.outputs.public_subnets
-  ip_whitelist   = concat(var.internal_ips, var.developer_ips, var.external_ips)
-  env            = var.env
+  ip_set         = concat(var.internal_ips, var.developer_ips, var.external_ips)
+  use_case       = var.use_case
+  scope          = var.scope
+  rules          = var.rules
+  universal_tags = var.universal_tags
+}
+
+
+
+variable "rules" {
+  description = "The rules to add to this WAF"
+  type        = list(object({}))
+  default     = []
+}
+
+variable "use_case" {
+  description = "Use case/resource for WAF"
+  type        = string
+  default     = "load_balancer"
+}
+
+variable "scope" {
+  description = "Scope of the WAF, either 'CLOUDFRONT' or 'REGIONAL'"
+  type        = string
+  default     = "REGIONAL"
+}
+
+variable "universal_tags" {
+  type        = map(string)
+  description = "Map to tag resources with"
 }
