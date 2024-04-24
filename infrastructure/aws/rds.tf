f6fd68090487ef4bc14718402a75907080595d1f
elliot.moore <elliotmoorecornwall@outlook.com>
2024-04-24 09:51:32 +0100

    Changed tfstate that kms comes from and added vpc_id to rds

--- infrastructure/aws/rds.tf
+++ infrastructure/aws/rds.tf
@@ -2,7 +2,7 @@ module "rds" {
     source                  = "../../../i-ai-core-infrastructure//modules/postgres"
     db_name                 = "${var.project_name}-psql-rds-${var.env}"
     developer_ips           = []
-    kms_secrets_arn         = data.terraform_remote_state.universal.outputs.kms_secrets_arn
+    kms_secrets_arn         = data.terraform_remote_state.platform.outputs.kms_key_arn
     private_subnet_ids_list = data.terraform_remote_state.vpc.outputs.private_subnets
     project                 = var.project_name
     public_subnet_ids_list  = data.terraform_remote_state.vpc.outputs.public_subnets
@@ -13,5 +13,5 @@ module "rds" {
     ]
     state_bucket = var.state_bucket
     task_prefix  = var.project_name
-    vpc_id       = data.terraform_remote_state.vpc.outputs
+    vpc_id       = data.terraform_remote_state.vpc.outputs.vpc_id
 }
