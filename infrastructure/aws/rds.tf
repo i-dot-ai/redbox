e2437d0406bb87191838e9ad04f46f032b026f22
elliot.moore <elliotmoorecornwall@outlook.com>
2024-04-24 08:34:59 +0100

    Formatted RDS module

--- infrastructure/aws/rds.tf
+++ infrastructure/aws/rds.tf
@@ -1,18 +1,17 @@
 module "rds" {
-    source = "../../../i-ai-core-infrastructure//modules/postgres"
-    db_name = "${var.project_name}-psql-rds-${var.env}"
-    developer_ips = []
-    kms_secrets_arn = data.terraform_remote_state.universal.outputs.kms_secrets_arn
+    source                  = "../../../i-ai-core-infrastructure//modules/postgres"
+    db_name                 = "${var.project_name}-psql-rds-${var.env}"
+    developer_ips           = []
+    kms_secrets_arn         = data.terraform_remote_state.universal.outputs.kms_secrets_arn
     private_subnet_ids_list = data.terraform_remote_state.vpc.outputs.private_subnets
-    project = var.project_name
-    public_subnet_ids_list = data.terraform_remote_state.vpc.outputs.public_subnets
-    service_sg_ids =
-    [
-      module.ingester.ecs_sg_id,
-      module.core_api.ecs_sg_id,
-      module.embedder.ecs_sg_id,
+    project                 = var.project_name
+    public_subnet_ids_list  = data.terraform_remote_state.vpc.outputs.public_subnets
+    service_sg_ids          = [
+        module.ingester.ecs_sg_id,
+        module.core_api.ecs_sg_id,
+        module.embedder.ecs_sg_id,
     ]
     state_bucket = var.state_bucket
-    task_prefix = var.project_name
-    vpc_id = data.terraform_remote_state.vpc.outputs
+    task_prefix  = var.project_name
+    vpc_id       = data.terraform_remote_state.vpc.outputs
 }
