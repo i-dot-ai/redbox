b2c0de41727d8acf1eed8e9a4fab891f532e1e22
George Burton <g.e.c.cburton@gmail.com>
2024-05-02 13:46:47 +0100

    remove test

--- core_api/tests/test_app.py
+++ core_api/tests/test_app.py
@@ -1,6 +1,3 @@
-from core_api.src.app import env
-
-
 def test_get_health(app_client):
     """
     Given that the app is running
@@ -8,48 +5,4 @@ def test_get_health(app_client):
     I Expect to see the docs
     """
     response = app_client.get("/health")
-    assert response.status_code == 200
-
-
-def test_read_model(client):
-    """
-    Given that I have a model in the database
-    When I GET /embedding_model
-    I Expect model-info to be returned
-    """
-    response = client.get("/embedding_model")
-    assert response.status_code == 200
-    assert response.json() == {
-        "embedding_model": env.embedding_model,
-        "vector_size": 768,
-    }
-
-
-def test_embed_sentences_422(client):
-    """
-    Given that I have a model in the database
-    When I POST a mall-formed payload to /embedding
-    I Expect a 422 error
-    """
-    response = client.post(
-        "/embedding",
-        json={"not": "a well formed payload"},
-    )
-    assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "Input should be a valid list"
-
-
-def test_embed_sentences(client):
-    """
-    Given that I have a model in the database
-    When I POST a valid payload consisting of some sentenced to embed to
-    /embedding
-    I Expect a 200 response
-
-    N.B. We are not testing the content / efficacy of the model in this test.
-    """
-    response = client.post(
-        "/embedding",
-        json=["I am the egg man", "I am the walrus"],
-    )
     assert response.status_code == 200
