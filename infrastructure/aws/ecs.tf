76d45c03fbcd4aacee4572040866fa5ecb6853c2
George Burton <8233643+gecBurton@users.noreply.github.com>
2024-05-02 11:00:45 +0100

    Feature/deploy django ecs (#292)

--- infrastructure/aws/ecs.tf
+++ infrastructure/aws/ecs.tf
@@ -1,8 +1,8 @@
 locals {
   record_prefix = terraform.workspace == "prod" ? var.project_name : "${var.project_name}-${terraform.workspace}"
-  host          = "${local.record_prefix}.${var.domain_name}"
+  django_host          = "${local.record_prefix}.${var.domain_name}"
+  api_host          = "${local.record_prefix}-api.${var.domain_name}"
 
-  prefix                = "backend"
   environment_variables = {
     "ELASTIC__API_KEY" : var.elastic_api_key,
     "ELASTIC__CLOUD_ID" : var.cloud_id,
@@ -13,12 +13,24 @@ locals {
     "INGEST_QUEUE_NAME" : "redbox-ingester-queue",
     "REDIS_HOST" : module.elasticache.redis_address,
     "REDIS_PORT" : module.elasticache.redis_port,
+#     django stuff
     "DJANGO_SECRET_KEY" : var.django_secret_key,
     "POSTGRES_PASSWORD" : var.postgres_password,
+    "POSTGRES_USER" : module.rds.rds_instance_username,
+    "POSTGRES_PASSWORD": module.rds.rds_instance_db_password,
+    "POSTGRES_DB" : module.rds.db_instance_name,
+    "POSTGRES_HOST" : module.rds.db_instance_address,
+    "CORE_API_HOST" : "https://${local.api_host}",
+    "CORE_API_PORT": 8000,
+    "ENVIRONMENT": upper(terraform.workspace),
+    "DJANGO_SETTINGS_MODULE": "redbox_app.settings",
+    "DEBUG": false,
+    "AWS_REGION": var.region,
     "OPENAI_API_KEY" : var.openai_api_key,
     "NOTIFY_API_KEY" : var.notify_api_key,
     "FROM_EMAIL" : var.from_email,
     "GOVUK_NOTIFY_API_KEY" : var.govuk_notify_plain_email_template_id,
+
   }
 }
 
@@ -28,9 +40,9 @@ module "cluster" {
   name           = "backend"
 }
 
-resource "aws_route53_record" "type_a_record" {
+resource "aws_route53_record" "type_a_record_core_api" {
   zone_id = var.hosted_zone_id
-  name    = local.host
+  name    = local.api_host
   type    = "A"
 
   alias {
@@ -41,13 +53,58 @@ resource "aws_route53_record" "type_a_record" {
 }
 
 
+resource "aws_route53_record" "type_a_record" {
+  zone_id = var.hosted_zone_id
+  name    = local.django_host
+  type    = "A"
+
+  alias {
+    name                   = module.load_balancer.load_balancer_dns_name
+    zone_id                = module.load_balancer.load_balancer_zone_id
+    evaluate_target_health = true
+  }
+}
+
+
+module "django-app" {
+  create_listener    = false
+  create_networking  = false
+  memory             = 4096
+  cpu                = 2048
+  source             = "../../../i-ai-core-infrastructure//modules/ecs"
+  project_name       = "redbox-django-app"
+  image_tag          = var.django_app_image_tag
+  prefix             = "frontend"
+  ecr_repository_uri = "${var.ecr_repository_uri}/redbox-django-app"
+  ecs_cluster_id     = module.cluster.ecs_cluster_id
+  health_check = {
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    accepted_response   = "200"
+    path                = "/health"
+    timeout             = 5
+  }
+  state_bucket                 = var.state_bucket
+  vpc_id                       = data.terraform_remote_state.vpc.outputs.vpc_id
+  private_subnets              = data.terraform_remote_state.vpc.outputs.private_subnets
+  container_port               = 8090
+  load_balancer_security_group = module.load_balancer.load_balancer_security_group_id
+  aws_lb_arn                   = module.load_balancer.alb_arn
+  host                         = local.django_host
+  ip_whitelist                 = var.external_ips
+  environment_variables        = local.environment_variables
+}
+
+
 module "core_api" {
-  create_networking  = true
+  memory             = 4096
+  cpu                = 2048
   create_listener    = true
+  create_networking  = true
   source             = "../../../i-ai-core-infrastructure//modules/ecs"
   project_name       = "redbox-core-api"
-  image_tag          = var.image_tag
-  prefix             = local.prefix
+  image_tag          = var.core_api_image_tag
+  prefix             = "backend"
   ecr_repository_uri = "${var.ecr_repository_uri}/redbox-core-api"
   ecs_cluster_id     = module.cluster.ecs_cluster_id
   health_check = {
@@ -63,7 +120,7 @@ module "core_api" {
   container_port               = 5002
   load_balancer_security_group = module.load_balancer.load_balancer_security_group_id
   aws_lb_arn                   = module.load_balancer.alb_arn
-  host                         = local.host
+  host                         = local.api_host
   ip_whitelist                 = var.external_ips
   environment_variables        = local.environment_variables
 }
@@ -75,8 +132,8 @@ module "worker" {
   cpu                = 2048
   source             = "../../../i-ai-core-infrastructure//modules/ecs"
   project_name       = "redbox-worker"
-  image_tag          = var.image_tag
-  prefix             = local.prefix
+  image_tag          = var.worker_image_tag
+  prefix             = "backend"
   ecr_repository_uri = "${var.ecr_repository_uri}/redbox-worker"
   ecs_cluster_id     = module.cluster.ecs_cluster_id
   health_check = {
@@ -92,7 +149,7 @@ module "worker" {
   container_port               = 5000
   load_balancer_security_group = module.load_balancer.load_balancer_security_group_id
   aws_lb_arn                   = module.load_balancer.alb_arn
-  host                         = local.host
+  host                         = local.api_host
   ip_whitelist                 = var.external_ips
   environment_variables        = local.environment_variables
 }
