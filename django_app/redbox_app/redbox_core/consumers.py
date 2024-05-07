aedf92430b624aada72ab333aa006815c9c625c7
Simon Brunning <simon@brunningonline.net>
2024-05-07 12:32:01 +0100

    Make consumer asynchronous.

--- django_app/redbox_app/redbox_core/consumers.py
+++ django_app/redbox_app/redbox_core/consumers.py
@@ -2,44 +2,58 @@ import json
 import logging
 from time import sleep
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
 from redbox_app.redbox_core.client import CoreApiClient
-from redbox_app.redbox_core.models import ChatHistory, ChatMessage, ChatRoleEnum
+from redbox_app.redbox_core.models import ChatHistory, ChatMessage, ChatRoleEnum, User
+from channels.db import database_sync_to_async
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel("DEBUG")
 
 
-class ChatConsumer(WebsocketConsumer):
-    def receive(self, text_data):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def receive(self, text_data):
         data = json.loads(text_data)
-        user = self.scope.get("user", None)
-        logger.debug(f"receive {text_data=} {user=}")
         user_message_text = data.get("message", "")
+        session_id = data.get("sessionId", None)
+        user: User = self.scope.get("user", None)
+        logger.debug(f"receive {user_message_text=} {session_id=} {user=}")
 
-        if session_id := data.get("sessionId", None):
+        session = await self.get_session(session_id, user, user_message_text)
+
+        # save user message
+        await self.save_message(session, user_message_text, ChatRoleEnum.user)
+
+        # get LLM response
+        session_messages = await self.get_messages(session)
+        message_history = [{"role": message.role, "text": message.text} for message in session_messages]
+        core_api = CoreApiClient(host=settings.CORE_API_HOST, port=settings.CORE_API_PORT)
+        ai_message_text = core_api.rag_chat(message_history, user.get_bearer_token())
+
+        # save LLM response
+        await self.save_message(session, ai_message_text, ChatRoleEnum.ai)
+
+        await self.send(ai_message_text)
+        sleep(0.5)
+        await self.send(" MESSAGE END")
+
+    @database_sync_to_async
+    def get_session(self, session_id: str, user: User, user_message_text: str):
+        if session_id:
             session = ChatHistory.objects.get(id=session_id)
         else:
             session_name = user_message_text[0:20]
             session = ChatHistory(name=session_name, users=user)
             session.save()
+        return session
 
-        # save user message
-        chat_message = ChatMessage(chat_history=session, text=user_message_text, role=ChatRoleEnum.user)
+    @database_sync_to_async
+    def get_messages(self, session):
+        return list(ChatMessage.objects.filter(chat_history=session))
+
+    @database_sync_to_async
+    def save_message(self, session, user_message_text, role):
+        chat_message = ChatMessage(chat_history=session, text=user_message_text, role=role)
         chat_message.save()
-
-        # get LLM response
-        message_history = [
-            {"role": message.role, "text": message.text} for message in ChatMessage.objects.filter(chat_history=session)
-        ]
-        core_api = CoreApiClient(host=settings.CORE_API_HOST, port=settings.CORE_API_PORT)
-        ai_message_text = core_api.rag_chat(message_history, user.get_bearer_token())
-
-        # save LLM response
-        llm_message = ChatMessage(chat_history=session, text=ai_message_text, role=ChatRoleEnum.ai)
-        llm_message.save()
-
-        self.send(ai_message_text)
-        sleep(0.5)
-        self.send(" MESSAGE END")
