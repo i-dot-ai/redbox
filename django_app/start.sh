05770f070605d1d72a6263f547c72743e57209a9
George Burton <g.e.c.cburton@gmail.com>
2024-05-10 14:24:18 +0100

    knda working locally

--- django_app/start.sh
+++ django_app/start.sh
@@ -2,6 +2,7 @@
 
 venv/bin/django-admin migrate
 venv/bin/django-admin collectstatic --noinput
+venv/bin/django-admin compress --force --engine jinja2
 
-venv/bin/django-admin runserver 0.0.0.0:8090
-#exec venv/bin/gunicorn -c ./redbox_app/gunicorn.py redbox_app.wsgi
+#venv/bin/django-admin runserver 0.0.0.0:8090
+exec venv/bin/gunicorn -c ./redbox_app/gunicorn.py redbox_app.wsgi
