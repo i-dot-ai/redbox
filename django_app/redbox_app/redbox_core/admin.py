5c35a462ca0d1eea5bf0b0037485b5a5fce78d28
Simon Brunning <simon@brunningonline.net>
2024-05-15 14:20:52 +0100

    Show links to files.

--- django_app/redbox_app/redbox_core/admin.py
+++ django_app/redbox_app/redbox_core/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+
+class FileResource(admin.ModelAdmin):
+    list_display = ["original_file_name", "user", "processing_status", "original_file", "core_file_uuid"]
+
+
 admin.site.register(models.User)
-admin.site.register(models.File)
+admin.site.register(models.File, FileResource)
