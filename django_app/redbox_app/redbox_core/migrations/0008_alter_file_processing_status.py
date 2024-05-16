5bf3351146cebbb75c810907dfb1618574e893dc
elliot.moore <elliotmoorecornwall@outlook.com>
2024-05-16 10:48:44 +0100

    Renamed duplicate migration

--- django_app/redbox_app/redbox_core/migrations/0008_alter_file_processing_status.py
+++ django_app/redbox_app/redbox_core/migrations/0008_alter_file_processing_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('redbox_core', '0006_file_core_file_uuid'),
+        ('redbox_core', '0007_alter_chathistory_options'),
     ]
 
     operations = [
