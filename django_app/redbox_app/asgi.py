6ee0714418cffaf0c7739a873683ec784cd857de
George Burton <8233643+gecBurton@users.noreply.github.com>
2024-05-16 18:31:37 +0100

    Revert "Feature/daphne-webserver (#385)"
    This reverts commit 9d0d1d25edc3d47966d6e53cd2456c7a60682e48.

--- django_app/redbox_app/asgi.py
+++ django_app/redbox_app/asgi.py
@@ -14,13 +14,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redbox_app.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+from .routing import websocket_urlpatterns
 
-# https://github.com/django/daphne/issues/347#issuecomment-733132711
-from redbox_app.routing import websocket_urlpatterns  # noqa: E402
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redbox_app.settings")
 
 application = ProtocolTypeRouter(
     {
