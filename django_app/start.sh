bc4bf4c15d10697771531fe595e69907a52dbf40
George Burton <g.e.c.cburton@gmail.com>
2024-05-16 10:39:36 +0100

    removed gunicorn again

--- django_app/start.sh
+++ django_app/start.sh
@@ -5,5 +5,4 @@ venv/bin/django-admin collectstatic --noinput
 venv/bin/django-admin compress --force --engine jinja2
 venv/bin/django-admin create_admin_user
 
-exec venv/bin/gunicorn -c ./redbox_app/gunicorn.py redbox_app.wsgi
 exec venv/bin/daphne -p 8090 redbox_app.asgi:application
