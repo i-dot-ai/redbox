8ddc526bc8b0693384c3c04706ecb83e4b9d1a02
Simon Brunning <simon@brunningonline.net>
2024-05-20 14:52:16 +0100

    Fix message saving.

--- django_app/redbox_app/redbox_core/consumers.py
+++ django_app/redbox_app/redbox_core/consumers.py
@@ -32,29 +32,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
         url = URL.build(scheme="ws", host=settings.CORE_API_HOST, port=settings.CORE_API_PORT) / "chat/rag-chat-stream"
         async with connect(str(url), extra_headers={"Authorization": user.get_bearer_token()}) as websocket:
             await websocket.send(json.dumps({"message_history": message_history}))
-            await self.send(json.dumps({"type": "session-id", "data": session.id}))
+            await self.send_json({"type": "session-id", "data": str(session.id)})
             full_reply: list[str] = []
             sources: list[File] = []
             async for raw_message in websocket:
                 message = json.loads(raw_message, object_hook=lambda d: SimpleNamespace(**d))
                 logger.debug("Received: %s", message)
                 if message.resource_type == "text":
-                    await self.send(json.dumps({"type": "text", "data": message.data}))
+                    await self.send_json({"type": "text", "data": message.data})
                     full_reply.append(message.data)
                 elif message.resource_type == "documents":
                     doc_uuids: list[str] = [doc.file_uuid for doc in message.data]
                     sources = File.objects.filter(core_file_uuid__in=doc_uuids, user=user)
                     for source in sources:
-                        await self.send(
-                            json.dumps(
-                                {
-                                    "type": "source",
-                                    "data": {"url": source.url, "original_file_name": source.original_file_name},
-                                }
-                            )
+                        await self.send_json(
+                            {
+                                "type": "source",
+                                "data": {"url": source.url, "original_file_name": source.original_file_name},
+                            }
                         )
                 elif message.resource_type == "end":
                     await self.save_message(session, "".join(full_reply), ChatRoleEnum.ai, sources)
+
+    async def send_json(self, data):
+        await self.send(json.dumps(data))
 
     @database_sync_to_async
     def get_session(self, session_id: str, user: User, user_message_text: str) -> ChatHistory:
@@ -74,8 +75,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def save_message(
         self, session: ChatHistory, user_message_text: str, role: ChatRoleEnum, source_files: list[File] | None = None
     ) -> ChatMessage:
-        chat_message = ChatMessage(
-            chat_history=session, text=user_message_text, role=role, source_files=source_files or []
-        )
+        chat_message = ChatMessage(chat_history=session, text=user_message_text, role=role)
         chat_message.save()
+        if source_files: chat_message.source_files.set(source_files)
         return chat_message
