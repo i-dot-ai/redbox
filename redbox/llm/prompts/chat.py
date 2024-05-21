8738149a593e0f27fe4129a7145c6eb3ba0d5355
George Burton <g.e.c.cburton@gmail.com>
2024-05-21 08:25:07 +0100

    momet sources altogether

--- redbox/llm/prompts/chat.py
+++ redbox/llm/prompts/chat.py
@@ -18,16 +18,7 @@ _with_sources_template = """Given the following extracted parts of a long docume
 a question, create a final answer with Sources at the end.  \
 If you don't know the answer, just say that you don't know. Don't try to make \
 up an answer.
-Be concise in your response and summarise where appropriate. \
-At the end of your response add a "Sources:" section with the documents you used. \
-DO NOT reference the source documents in your response. Only cite at the end. \
-ONLY PUT CITED DOCUMENTS IN THE "Sources:" SECTION AND NO WHERE ELSE IN YOUR RESPONSE. \
-IT IS CRUCIAL that citations only happens in the "Sources:" section. \
-This format should be <DocX> where X is the document UUID being cited.  \
-DO NOT INCLUDE ANY DOCUMENTS IN THE "Sources:" THAT YOU DID NOT USE IN YOUR RESPONSE. \
-YOU MUST CITE USING THE <DocX> FORMAT. NO OTHER FORMAT WILL BE ACCEPTED.
-Example: "Sources: <DocX> <DocY> <DocZ>"
-If no DOCUMENTS are CITED then omit the Sources section.
+Be concise in your response and summarise where appropriate.
 
 Use **bold** to highlight the most question relevant parts in your response.
 If dealing dealing with lots of data return it in markdown table format.
