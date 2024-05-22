ae92eabfc36ad7c523374dd5745e04a6c124d1ea
Simon Brunning <simon@brunningonline.net>
2024-05-22 15:27:37 +0100

    @override decorator doesn't exist in Python 3.11

--- django_app/redbox_app/redbox_core/models.py
+++ django_app/redbox_app/redbox_core/models.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import timedelta
-from typing import override
 
 import boto3
 from botocore.config import Config
@@ -81,8 +80,7 @@ class File(UUIDPrimaryKeyBase, TimeStampedModel):
                 self.expiry_date = timezone.now() + timedelta(seconds=settings.FILE_EXPIRY_IN_SECONDS)
         super().save(*args, **kwargs)
 
-    @override
-    def delete(self, using=None, keep_parents=False):
+    def delete(self, using=None, keep_parents=False):  # noqa: ARG002  # remove at Python 3.12
         #  Needed to make sure no orphaned files remain in the storage
         self.original_file.storage.delete(self.original_file.name)
         super().delete()
