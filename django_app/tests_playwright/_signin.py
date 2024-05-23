670414b2ac0ce6438b85839583309cb59d3dfd32
Will Langdale <will.langdale@trade.gov.uk>
2024-05-23 17:55:19 +0100

    Placated ruff

--- django_app/tests_playwright/_signin.py
+++ django_app/tests_playwright/_signin.py
@@ -1,21 +1,22 @@
 import os
 import subprocess
-from playwright.sync_api import Page, expect
-from dotenv import load_dotenv
 from pathlib import Path
-from _settings import BASE_URL
 
+from _settings import BASE_URL
+from dotenv import load_dotenv
+from playwright.sync_api import Page, expect
 
 ROOT = Path(__file__).parents[1]
 load_dotenv(dotenv_path=ROOT / ".env", override=True)
 
 
 def sign_in(page: Page):
-
     email_address = os.environ["USER_EMAIL"]
 
     if not email_address:
-        raise Exception("USER_EMAIL not set in your .env - this must be set to the email address you use for signing in.")
+        raise Exception(
+            "USER_EMAIL not set in your .env - this must be set to the email address you use for signing in."
+        )
 
     # Sign in page
     page.goto(f"{BASE_URL}/sign-in/")
@@ -24,12 +25,10 @@ def sign_in(page: Page):
     page.get_by_text("Continue").click()
 
     # Get magic link
-    current_script_path = os.path.abspath(__file__)
-    django_dir = os.path.dirname(os.path.dirname(current_script_path))
     command = ["poetry", "run", "python", "manage.py", "show_magiclink_url", email_address]
-    result = subprocess.run(command, capture_output=True, text=True, cwd=django_dir)
+    result = subprocess.run(command, capture_output=True, text=True, cwd=ROOT, shell=False)  # noqa S603
     magic_link = result.stdout.strip()
-    
+
     # Complete sign-in and verify
     page.goto(f"{BASE_URL}{magic_link}")
     page.get_by_role("button").click()
--- django_app/tests_playwright/test_accessibility.py
+++ django_app/tests_playwright/test_accessibility.py
@@ -1,35 +1,46 @@
-from playwright.sync_api import Page, expect
-from axe_playwright_python.sync_playwright import Axe
+import logging
+
 from _settings import BASE_URL
 from _signin import sign_in
+from axe_playwright_python.sync_playwright import Axe
+from playwright.sync_api import Page, expect
 
-URLS = [
-    "/",
-    "/sign-in",
-    "/privacy-notice",
-    "/accessibility-statement",
-    "/support",
-    "/documents",
-    "/upload",
-    "/chats"
-]
+URLS = ["/", "/sign-in", "/privacy-notice", "/accessibility-statement", "/support", "/documents", "/upload", "/chats"]
 
 # All available rules/categories are here: https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
 # Can't include all as gov.uk design system violates the "region" rule
-AXE_OPTIONS = {"runOnly": {"type": "tag", "values": ["wcag2a", "wcag2aa", "wcag21aa", "wcag22aa", "cat.aria", "cat.name-role-value", "cat.structure", "cat.semantics", "cat.text-alternatives", "cat.forms", "cat.sensory-and-visual-cues", "cat.tables", "cat.time-and-media"]}}
+AXE_OPTIONS = {
+    "runOnly": {
+        "type": "tag",
+        "values": [
+            "wcag2a",
+            "wcag2aa",
+            "wcag21aa",
+            "wcag22aa",
+            "cat.aria",
+            "cat.name-role-value",
+            "cat.structure",
+            "cat.semantics",
+            "cat.text-alternatives",
+            "cat.forms",
+            "cat.sensory-and-visual-cues",
+            "cat.tables",
+            "cat.time-and-media",
+        ],
+    }
+}
 
 axe = Axe()
 
-def test_violations(page: Page):
 
+def test_violations(page: Page):
     sign_in(page)
 
     for url in URLS:
-
         page.goto(f"{BASE_URL}{url}")
         results = axe.run(page, context=None, options=AXE_OPTIONS)
-        print(f"\nURL: {url}")
-        print(results.generate_report())
+        logging.info("\nURL: %s", url)
+        logging.info(results.generate_report())
 
         if results.violations_count > 0:
             # Because Python Playwright assertions can't take normal expressions, booleans etc.
