929d927e426cc63069dc0fda91131937f84f37e3
Rachael Robinson <23265724+rachaelcodes@users.noreply.github.com>
2024-05-23 09:34:27 +0100

    fix test after rebase

--- django_app/tests/test_models.py
+++ django_app/tests/test_models.py
@@ -1,29 +1,12 @@
-from datetime import UTC, datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import pytest
-
-from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from redbox_app.redbox_core.models import File, ProcessingStatusEnum
 
-@pytest.mark.django_db
-def test_file_model_expiry_date(uploaded_file: File):
-    # Tests the initial value of the expiry_date
-    expected_expiry_date = uploaded_file.created_at + timedelta(seconds=settings.FILE_EXPIRY_IN_SECONDS)
-    # The expiry_date should be FILE_EXPIRY_IN_SECONDS ahead of created_at
-    # these fields are set during the same save() process
-    # this test accounts for a delay between creating the fields
-    assert abs(uploaded_file.expiry_date - expected_expiry_date) < timedelta(seconds=1)
-
-    # Tests that the expiry_date can be updated
-    new_date = datetime(2028, 1, 1, tzinfo=UTC)
-    uploaded_file.expiry_date = new_date
-    uploaded_file.save()
-    assert uploaded_file.expiry_date == new_date
-
 
 @pytest.mark.django_db
-def test_file_model_last_referenced(peter_rabbit, s3_client):
+def test_file_model_last_referenced(peter_rabbit, s3_client):  # noqa: ARG001
     mock_file = SimpleUploadedFile("test.txt", b"these are the file contents")
 
     new_file = File.objects.create(
@@ -41,7 +24,7 @@ def test_file_model_last_referenced(peter_rabbit, s3_client):
     assert abs(new_file.last_referenced - expected_last_referenced) < timedelta(seconds=1)
 
     # Tests that the last_referenced field can be updated
-    new_date = datetime(2028, 1, 1)
+    new_date = datetime(2028, 1, 1, tzinfo=timezone.utc)
     new_file.last_referenced = new_date
     new_file.save()
     assert new_file.last_referenced == new_date
