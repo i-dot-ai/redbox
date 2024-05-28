61744522b2a59d0a70ebd52a3ca69acbcc945383
Kevin Etchells <kevetchells@hotmail.com>
2024-05-28 15:10:10 +0100

    Fix linting errors

--- django_app/tests_playwright/test_info_pages.py
+++ django_app/tests_playwright/test_info_pages.py
@@ -3,15 +3,14 @@ from tests_playwright.pages import LandingPage
 
 
 def test_support_pages(page: Page):
-
     # Landing page
     landing_page = LandingPage(page)
-    
+
     # Privacy page
-    privacy_page = landing_page.navigate_to_privacy_page()
+    landing_page.navigate_to_privacy_page()
 
     # Accessibility page
-    accessibility_page = landing_page.navigate_to_accessibility_page()
+    landing_page.navigate_to_accessibility_page()
 
     # Support page
-    support_page = landing_page.navigate_to_support_page()
+    landing_page.navigate_to_support_page()
