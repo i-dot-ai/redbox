db633ef8e425eb6faa542c4fd2712f0caae9172a
Will Langdale <will.langdale@trade.gov.uk>
2024-05-30 09:22:02 +0100

    Changed match to match_phrase in elastic to accept only exact matches

--- redbox/storage/elasticsearch.py
+++ redbox/storage/elasticsearch.py
@@ -96,7 +96,7 @@ class ElasticsearchStorageHandler(BaseStorageHandler):
             result = scan(
                 client=self.es_client,
                 index=target_index,
-                query={"query": {"match": {"creator_user_uuid": str(user_uuid)}}},
+                query={"query": {"match_phrase": {"creator_user_uuid": str(user_uuid)}}},
                 _source=True,
             )
 
@@ -126,7 +126,7 @@ class ElasticsearchStorageHandler(BaseStorageHandler):
             results = scan(
                 client=self.es_client,
                 index=target_index,
-                query={"query": {"match": {"creator_user_uuid": str(user_uuid)}}},
+                query={"query": {"match_phrase": {"creator_user_uuid": str(user_uuid)}}},
                 _source=False,
             )
 
@@ -149,12 +149,12 @@ class ElasticsearchStorageHandler(BaseStorageHandler):
                         "bool": {
                             "must": [
                                 {
-                                    "match": {
+                                    "match_phrase": {
                                         "parent_file_uuid": str(parent_file_uuid),
                                     }
                                 },
                                 {
-                                    "match": {
+                                    "match_phrase": {
                                         "creator_user_uuid": str(user_uuid),
                                     }
                                 },
