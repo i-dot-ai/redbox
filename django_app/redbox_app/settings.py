26b9d0b6b5088de850fa3045e24b80af7fc47443
George Burton <g.e.c.cburton@gmail.com>
2024-05-31 16:43:57 +0100

    no default scheme

--- django_app/redbox_app/settings.py
+++ django_app/redbox_app/settings.py
@@ -16,7 +16,7 @@ env = environ.Env()
 
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 ENVIRONMENT = env.str("ENVIRONMENT")
-WEBSOCKET_SCHEME = env.str("WEBSOCKET_SCHEME")
+WEBSOCKET_SCHEME = env.str("WEBSOCKET_SCHEME", "wss")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
