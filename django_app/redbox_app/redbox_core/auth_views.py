de73439a6ae794fc93af3b4cf8cf8002a469c16c
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2024-06-04 09:07:23 +0100

    make sso configurable

--- django_app/redbox_app/redbox_core/auth_views.py
+++ django_app/redbox_app/redbox_core/auth_views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.conf import settings
 from django.contrib.auth import logout
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
@@ -14,8 +15,38 @@ logger = logging.getLogger(__name__)
 def sign_in_view(request: HttpRequest):
     if request.user.is_authenticated:
         return redirect("homepage")
-    
-    return redirect("/auth/login")
+    if settings.LOGIN_METHOD == "sso":
+        return redirect("/auth/login")
+    if request.method == "POST":
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"].lower()
+
+            try:
+                user = models.User.objects.get(email=email)
+                link = MagicLink.objects.create(user=user, redirect_to="/documents")
+                full_link = request.build_absolute_uri(link.get_absolute_url())
+                email_handler.send_magic_link_email(full_link, email)
+            except models.User.DoesNotExist as e:
+                logger.debug("User with email %s not found", email, exc_info=e)
+            except HTTPError as e:
+                logger.exception("failed to send link to %s", email, exc_info=e)
+
+            return redirect("sign-in-link-sent")
+
+        return render(
+            request,
+            template_name="sign-in.html",
+            context={
+                "errors": form.errors,
+            },
+        )
+
+    return render(
+        request,
+        template_name="sign-in.html",
+        context={"request": request},
+    )
 
 
 def sign_in_link_sent_view(request: HttpRequest):
