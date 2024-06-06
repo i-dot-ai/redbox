5aaa5aed1199996166504ce254c2f8ed47535e9a
elliot.moore <elliotmoorecornwall@outlook.com>
2024-06-06 21:26:13 +0100

    Formatting

--- django_app/redbox_app/settings.py
+++ django_app/redbox_app/settings.py
@@ -6,10 +6,9 @@ from pathlib import Path
 import environ
 import sentry_sdk
 from dotenv import load_dotenv
+from redbox_app.setting_enums import Classification, Environment
 from sentry_sdk.integrations.django import DjangoIntegration
 from storages.backends import s3boto3
-
-from redbox_app.setting_enums import Classification, Environment
 
 load_dotenv()
 
