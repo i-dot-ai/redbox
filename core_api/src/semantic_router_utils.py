7d4ae5855ee742c1824b4fa2942df7b4c204b371
Andy Symonds <andrew.symonds@thoughtworks.com>
2024-06-07 14:58:25 +0100

    [semantic-routing] | AS | Refactored build_chain logic and added coach and ability routes

---
+++ core_api/src/semantic_router_utils.py
@@ -0,0 +1,20 @@
+info_text = """
+    I am RedBox, an AI focused on helping UK Civil Servants, Political Advisors and\
+    Ministers triage and summarise information from a wide variety of sources.
+    """
+
+ability_text = """
+    I am help you search over selected documents and do Q&A on them.\
+    I can help you summarise selected documents.\
+    I can help you extract information from selected documents.\
+    I can return information in a variety of formats, such as bullet points.
+    """
+
+coach_text = """
+    I am sorry that didn't work.\
+    You could try rephrasing your task, i.e if you want to summarise a document\
+    please use the term, "Summarise the selected document" or "extract all action\
+    items from the selected document."
+    If you want the results to be returned in a specific format, please specify the\
+    format in as much detail as possible.
+    """
