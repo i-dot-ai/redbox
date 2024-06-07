fa4c56bc5fa519e91d8ad710215217ec2f6e3ad6
Andy Symonds <andrew.symonds@thoughtworks.com>
2024-06-07 23:38:57 +0100

    [semantic-routing] | AS | Add test for non-streaming chat/rag endpoint

--- core_api/tests/routes/test_chat.py
+++ core_api/tests/routes/test_chat.py
@@ -3,6 +3,7 @@ from types import SimpleNamespace
 from typing import TYPE_CHECKING
 from unittest.mock import AsyncMock, MagicMock, patch
 
+import pytest
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompt_values import ChatPromptValue
 from langchain_core.runnables import Runnable
@@ -21,6 +22,11 @@ test_history = [
     ([system_chat, system_chat], 422),
     # ([system_chat, user_chat], 200), TODO: restore this test
 ]
+
+# user_gratitude = {"message_history": [{"role": "user", "text": "Thank you"}]}
+user_gratitude = {"role": "user", "text": "Thank you"}
+test_gratitude = [([user_gratitude])]
+# user_gratitude = {"message_history": [{"role": "user", "text": "Thank you"}]}
 
 
 def mock_chat_prompt():
@@ -44,11 +50,24 @@ def mock_get_chain():
 
 # @pytest.mark.parametrize(("chat_history", "status_code"), test_history)
 # def test_simple_chat(chat_history, status_code, app_client, monkeypatch, headers):
-#     monkeypatch.setattr("langchain_core.prompts.ChatPromptTemplate.from_messages", mock_chat_prompt)
+#     monkeypatch.setattr(
+#         "langchain_core.prompts.ChatPromptTemplate.from_messages", mock_chat_prompt
+#     )
 #     monkeypatch.setattr("core_api.src.routes.chat.LLMChain", mock_get_chain)
 
-#     response = app_client.post("/chat/vanilla", json={"message_history": chat_history}, headers=headers)
+#     response = app_client.post(
+#         "/chat/vanilla", json={"message_history": chat_history}, headers=headers
+#     )
 #     assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("chat_history", test_gratitude)
+def test_rag_chat(chat_history, app_client, headers):
+    response = app_client.post(
+        "/chat/rag", json={"message_history": chat_history}, headers=headers
+    )
+    response_dict = response.json()
+    assert response_dict["output_text"] == "You're welcome!"
 
 
 def test_rag_chat_streamed(app_client, headers):
@@ -71,7 +90,11 @@ def test_rag_chat_streamed(app_client, headers):
         StreamEvent(
             event="on_chat_model_stream",
             name="event-2",
-            data={"chunk": SimpleNamespace(content="by the American songwriter Barry Mann.")},
+            data={
+                "chunk": SimpleNamespace(
+                    content="by the American songwriter Barry Mann."
+                )
+            },
             run_id="run_id",
         ),
     ]
@@ -79,7 +102,9 @@ def test_rag_chat_streamed(app_client, headers):
     build_retrieval_chain = mock_build_retrieval_chain(events)
 
     with (
-        patch("core_api.src.routes.chat.build_retrieval_chain", new=build_retrieval_chain),
+        patch(
+            "core_api.src.routes.chat.build_retrieval_chain", new=build_retrieval_chain
+        ),
         app_client.websocket_connect("/chat/rag", headers=headers) as websocket,
     ):
         # When
