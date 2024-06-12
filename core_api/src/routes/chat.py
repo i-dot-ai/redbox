1d6cf0b5489eb63425ad506d96a1741a10b63c51
Will Langdale <will.langdale@trade.gov.uk>
2024-06-12 18:35:20 +0100

    Added summarisation to core api in live

--- core_api/src/routes/chat.py
+++ core_api/src/routes/chat.py
@@ -1,35 +1,29 @@
 import logging
-from http import HTTPStatus
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import Depends, FastAPI, HTTPException, WebSocket
+from fastapi import Depends, FastAPI, WebSocket
 from fastapi.encoders import jsonable_encoder
-from langchain.chains.llm import LLMChain
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain_community.chat_models import ChatLiteLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_elasticsearch import ElasticsearchStore
 
 from core_api.src.auth import get_user_uuid, get_ws_user_uuid
-from core_api.src.dependencies import get_llm, get_vector_store
+from core_api.src.build_chains import build_retrieval_chain, build_stuff_chain
+from core_api.src.dependencies import get_llm, get_storage_handler, get_vector_store
 from core_api.src.semantic_routes import (
     ABILITY_RESPONSE,
     COACH_RESPONSE,
     INFO_RESPONSE,
     route_layer,
 )
-from redbox.llm.prompts.chat import (
-    CONDENSE_QUESTION_PROMPT,
-    STUFF_DOCUMENT_PROMPT,
-    WITH_SOURCES_PROMPT,
-)
 from redbox.models.chat import ChatRequest, ChatResponse, SourceDocument
+from redbox.storage import ElasticsearchStorageHandler
 
 # === Logging ===
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
 
 
 chat_app = FastAPI(
@@ -54,82 +48,36 @@ ROUTE_RESPONSES = {
     "ability": ChatPromptTemplate.from_template(ABILITY_RESPONSE),
     "coach": ChatPromptTemplate.from_template(COACH_RESPONSE),
     "gratitude": ChatPromptTemplate.from_template("You're welcome!"),
-    "summarisation": ChatPromptTemplate.from_template("You are asking for summarisation - route not yet implemented"),
+    "summarisation": build_stuff_chain,
     "extract": ChatPromptTemplate.from_template("You asking to extract some information - route not yet implemented"),
 }
 
 
-async def build_vanilla_chain(
+async def semantic_router_to_chain(
     chat_request: ChatRequest,
-) -> ChatPromptTemplate:
-    """Get a LLM response to a question history"""
-
-    if len(chat_request.message_history) < 2:  # noqa: PLR2004
-        raise HTTPException(
-            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-            detail="Chat history should include both system and user prompts",
-        )
-
-    if chat_request.message_history[0].role != "system":
-        raise HTTPException(
-            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-            detail="The first entry in the chat history should be a system prompt",
-        )
-
-    if chat_request.message_history[-1].role != "user":
-        raise HTTPException(
-            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-            detail="The final entry in the chat history should be a user question",
-        )
-
-    return ChatPromptTemplate.from_messages((msg.role, msg.text) for msg in chat_request.message_history)
-
-
-async def build_retrieval_chain(
-    chat_request: ChatRequest, user_uuid: UUID, llm: ChatLiteLLM, vector_store: ElasticsearchStore
+    user_uuid: UUID,
+    llm: ChatLiteLLM,
+    vector_store: ElasticsearchStore,
+    storage_handler: ElasticsearchStorageHandler,
 ):
-    question = chat_request.message_history[-1].text
-    previous_history = list(chat_request.message_history[:-1])
-    previous_history = ChatPromptTemplate.from_messages(
-        (msg.role, msg.text) for msg in previous_history
-    ).format_messages()
-
-    docs_with_sources_chain = load_qa_with_sources_chain(
-        llm,
-        chain_type="stuff",
-        prompt=WITH_SOURCES_PROMPT,
-        document_prompt=STUFF_DOCUMENT_PROMPT,
-        verbose=True,
-    )
-
-    condense_question_chain = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
-
-    standalone_question = condense_question_chain({"question": question, "chat_history": previous_history})["text"]
-
-    search_kwargs = {"filter": {"bool": {"must": [{"term": {"creator_user_uuid.keyword": str(user_uuid)}}]}}}
-
-    if chat_request.selected_files:
-        logging.info("chat_request.selected_files: %s", str(chat_request.selected_files))
-        search_kwargs["filter"]["bool"]["must"] = [
-            {"term": {"parent_file_uuid.keyword": str(file.uuid)}} for file in chat_request.selected_files
-        ]
-
-    docs = vector_store.as_retriever(search_kwargs=search_kwargs).get_relevant_documents(standalone_question)
-
-    params = {
-        "question": standalone_question,
-        "input_documents": docs,
-    }
-
-    return docs_with_sources_chain, params
-
-
-async def build_chain(chat_request: ChatRequest, user_uuid: UUID, llm: ChatLiteLLM, vector_store: ElasticsearchStore):
     question = chat_request.message_history[-1].text
     route = route_layer(question)
 
     if route_response := ROUTE_RESPONSES.get(route.name):
-        return route_response, {}
+        # check if route_response is an instance of ChatPromptTemplate
+        if isinstance(route_response, ChatPromptTemplate):
+            return route_response, {}
+        if callable(route_response):
+            build_chain = route_response
+            chain, params = await build_chain(
+                chat_request=chat_request,
+                user_uuid=user_uuid,
+                llm=llm,
+                vector_store=vector_store,
+                storage_handler=storage_handler,
+            )
+            return chain, params  # params for summarisation
+
     # build_vanilla_chain could go here
 
     # RAG chat
@@ -153,11 +101,9 @@ async def rag_chat(
         StreamingResponse: a stream of the chain response
     """
 
-    logging.info("chat_request: %s", chat_request)
-
     question = chat_request.message_history[-1].text
     route = route_layer(question)
-
+    # TODO (@wpfl-dbt): will need updating - focused on streaming endpoint  # noqa: TD003
     if route_response := ROUTE_RESPONSES.get(route.name):
         response = route_response.invoke({})
         return ChatResponse(output_text=response.messages[0].content)
@@ -185,17 +131,16 @@ async def rag_chat_streamed(
     websocket: WebSocket,
     llm: Annotated[ChatLiteLLM, Depends(get_llm)],
     vector_store: Annotated[ElasticsearchStore, Depends(get_vector_store)],
+    storage_handler: Annotated[ElasticsearchStorageHandler, Depends(get_storage_handler)],
 ):
     await websocket.accept()
 
     user_uuid = await get_ws_user_uuid(websocket)
 
     request = await websocket.receive_text()
-    logger.debug("raw request from django-app: %s", request)
     chat_request = ChatRequest.model_validate_json(request)
-    logger.debug("chat request from django-app: %s", chat_request)
 
-    chain, params = await build_chain(chat_request, user_uuid, llm, vector_store)
+    chain, params = await semantic_router_to_chain(chat_request, user_uuid, llm, vector_store, storage_handler)
 
     async for event in chain.astream_events(params, version="v1"):
         kind = event["event"]
@@ -204,22 +149,24 @@ async def rag_chat_streamed(
         elif kind == "on_chat_model_end":
             await websocket.send_json({"resource_type": "end"})
         elif kind == "on_chain_stream":
-            source_documents = [
-                jsonable_encoder(
-                    SourceDocument(
-                        page_content=document.page_content,
-                        file_uuid=document.metadata.get("parent_doc_uuid"),
-                        page_numbers=document.metadata.get("page_numbers"),
+            if isinstance(event["data"]["chunk"], dict):
+                input_documents = event["data"]["chunk"].get("input_documents", [])
+                source_documents = [
+                    jsonable_encoder(
+                        SourceDocument(
+                            page_content=document.page_content,
+                            file_uuid=document.metadata.get("parent_doc_uuid"),
+                            page_numbers=document.metadata.get("page_numbers"),
+                        )
                     )
-                )
-                for document in event["data"]["chunk"].get("input_documents", [])
-            ]
-            await websocket.send_json({"resource_type": "documents", "data": source_documents})
+                    for document in input_documents
+                ]
+                await websocket.send_json({"resource_type": "documents", "data": source_documents})
         elif kind == "on_prompt_stream":
             try:
                 msg = event["data"]["chunk"].messages[0].content
                 await websocket.send_json({"resource_type": "text", "data": msg})
             except (KeyError, AttributeError):
-                logger.exception("unknown message format %s", str(event["data"]["chunk"]))
+                logging.exception("unknown message format %s", str(event["data"]["chunk"]))
 
     await websocket.close()
--- core_api/tests/test_runnables.py
+++ core_api/tests/test_runnables.py
@@ -1,17 +1,11 @@
 import re
 
-import pytest
-
 from core_api.src.format import format_chunks, get_file_chunked_to_tokens
-from core_api.src.routes.chat import build_retrieval_chain
 from core_api.src.runnables import (
-    make_condense_question_runnable,
-    make_condense_rag_runnable,
     make_es_retriever,
     make_rag_runnable,
     make_stuff_document_runnable,
 )
-from redbox.models import ChatRequest
 
 
 def test_format_chunks(stored_file_chunks):
@@ -63,36 +57,25 @@ def test_make_stuff_document_runnable(mock_llm, stored_file_chunks):
     assert response == "<<TESTING>>"
 
 
-@pytest.mark.asyncio()
-async def test_build_retrieval_chain(mock_llm, chunked_file, other_stored_file_chunks, vector_store):  # noqa: ARG001
-    request = {
-        "message_history": [
-            {"text": "hello", "role": "user"},
-        ],
-        "selected_files": [{"uuid": chunked_file.uuid}],
-    }
-
-    docs_with_sources_chain, params = await build_retrieval_chain(
-        chat_request=ChatRequest(**request),
-        user_uuid=chunked_file.creator_user_uuid,
-        llm=mock_llm,
-        vector_store=vector_store,
-    )
-
-    assert all(doc.metadata["parent_doc_uuid"] == str(chunked_file.uuid) for doc in params["input_documents"])
-
-
 def test_make_es_retriever(es_client, embedding_model, chunked_file, chunk_index_name):
     retriever = make_es_retriever(es=es_client, embedding_model=embedding_model, chunk_index_name=chunk_index_name)
 
     one_doc_chunks = retriever.invoke(
-        input={"question": "hello", "file_uuids": [chunked_file.uuid], "user_uuid": chunked_file.creator_user_uuid}
+        input={
+            "question": "hello",
+            "file_uuids": [chunked_file.uuid],
+            "user_uuid": chunked_file.creator_user_uuid,
+        }
     )
 
     assert {chunked_file.uuid} == {chunk.parent_file_uuid for chunk in one_doc_chunks}
 
     no_doc_chunks = retriever.invoke(
-        input={"question": "tell me about energy", "file_uuids": [], "user_uuid": chunked_file.creator_user_uuid}
+        input={
+            "question": "tell me about energy",
+            "file_uuids": [],
+            "user_uuid": chunked_file.creator_user_uuid,
+        }
     )
 
     assert len(no_doc_chunks) >= 1
@@ -102,49 +85,6 @@ def test_make_rag_runnable(es_client, embedding_model, chunk_index_name, mock_ll
     retriever = make_es_retriever(es=es_client, embedding_model=embedding_model, chunk_index_name=chunk_index_name)
 
     chain = make_rag_runnable(system_prompt="Your job is Q&A.", llm=mock_llm, retriever=retriever)
-
-    previous_history = [
-        {"text": "Lorem ipsum dolor sit amet.", "role": "user"},
-        {"text": "Consectetur adipiscing elit.", "role": "ai"},
-        {"text": "Donec cursus nunc tortor.", "role": "user"},
-    ]
-
-    response = chain.invoke(
-        input={
-            "question": "Who are all these people?",
-            "messages": [(msg["role"], msg["text"]) for msg in previous_history],
-            "file_uuids": [chunked_file.uuid],
-            "user_uuid": chunked_file.creator_user_uuid,
-        }
-    )
-
-    assert response["response"] == "<<TESTING>>"
-    assert {chunked_file.uuid} == {chunk.parent_file_uuid for chunk in response["sources"]}
-
-
-def test_make_condense_question_runnable(mock_llm):
-    chain = make_condense_question_runnable(llm=mock_llm)
-
-    previous_history = [
-        {"text": "Lorem ipsum dolor sit amet.", "role": "user"},
-        {"text": "Consectetur adipiscing elit.", "role": "ai"},
-        {"text": "Donec cursus nunc tortor.", "role": "user"},
-    ]
-
-    response = chain.invoke(
-        input={
-            "question": "How are you today?",
-            "messages": [(msg["role"], msg["text"]) for msg in previous_history],
-        }
-    )
-
-    assert response == "<<TESTING>>"
-
-
-def test_make_condense_rag_runnable(es_client, embedding_model, chunk_index_name, mock_llm, chunked_file):
-    retriever = make_es_retriever(es=es_client, embedding_model=embedding_model, chunk_index_name=chunk_index_name)
-
-    chain = make_condense_rag_runnable(system_prompt="Your job is Q&A.", llm=mock_llm, retriever=retriever)
 
     previous_history = [
         {"text": "Lorem ipsum dolor sit amet.", "role": "user"},
