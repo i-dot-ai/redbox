d8951455c6b2f97de3021b139599845248ab0e55
Liam Wilkinson <liam.wilkinson@cabinetoffice.gov.uk>
2024-06-24 16:53:35 +0100

    Synchronising version to pyproject

--- redbox/__init__.py
+++ redbox/__init__.py
@@ -1,3 +1,10 @@
 """Redbox is a Python library for working with the Redbox API, data and services."""
 
-__version__ = "0.9.0"
+# Get version from poetry pyproject.toml
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
