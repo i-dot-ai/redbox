b1a08009eeded29a231537f4ad0cb149d04c91e0
Liam Wilkinson <liam.wilkinson@cabinetoffice.gov.uk>
2024-06-24 17:13:39 +0100

    Propagate version to API status

--- redbox/__init__.py
+++ redbox/__init__.py
@@ -1,10 +1,3 @@
 """Redbox is a Python library for working with the Redbox API, data and services."""
 
-# Get version from poetry pyproject.toml
-
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    __version__ = "unknown"
+__version__ = "0.0.3"
