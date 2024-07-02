a6ec8f25e30250f012beec336c56f3c6ecaa7328
George Burton <g.e.c.cburton@gmail.com>
2024-07-02 09:19:21 +0100

    renamed redbox-copilot to rebox-root

--- redbox_copilot/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file/folder only exists because poetry insists
-# that a folder that matches the project-name exists and
-# the name the project cannot be called redbox because
-# one of its dependencies is called redbox.
-#
-# A better solution will be found!
