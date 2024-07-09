1d77078c5304ec96635da8064cd98b23a5dae543
Kevin Etchells <kevetchells@hotmail.com>
2024-07-09 08:21:54 +0100

    Implement new route-display design

--- django_app/frontend/js/trusted-types.js
+++ django_app/frontend/js/trusted-types.js
@@ -3,18 +3,20 @@
 /** @type {import ('../node_modules/@types/dompurify/index.d.ts')} */
 let DOMPurify = window["DOMPurify"];
 
-
 // Create default policy
-if (typeof(window.trustedTypes) !== 'undefined') {
-    window.trustedTypes.createPolicy('default', {
-        createHTML: (to_escape) =>
-            DOMPurify.sanitize(to_escape, { 
-                RETURN_TRUSTED_TYPE: false,
-                CUSTOM_ELEMENT_HANDLING: {
-                    tagNameCheck: (tagName) => tagName === 'markdown-converter' || tagName === 'sources-list',
-                    attributeNameCheck: (attr) => true,
-                    allowCustomizedBuiltInElements: true
-                }
-            }),
-    });
+if (typeof window.trustedTypes !== "undefined") {
+  window.trustedTypes.createPolicy("default", {
+    createHTML: (to_escape) =>
+      DOMPurify.sanitize(to_escape, {
+        RETURN_TRUSTED_TYPE: false,
+        CUSTOM_ELEMENT_HANDLING: {
+          tagNameCheck: (tagName) =>
+            tagName === "markdown-converter" ||
+            tagName === "sources-list" ||
+            tagName === "tool-tip",
+          attributeNameCheck: (attr) => true,
+          allowCustomizedBuiltInElements: true,
+        },
+      }),
+  });
 }
