b49fd1e8fce18103f01b1118ba1bc360fd7741ae
George Burton <g.e.c.cburton@gmail.com>
2024-07-16 07:18:00 +0100

    create index if it doesnt exist

--- core-api/tests/conftest.py
+++ core-api/tests/conftest.py
@@ -50,7 +50,8 @@ def es_index(env) -> str:
 @pytest.fixture(autouse=True)
 def create_index(env, es_index):
     es: Elasticsearch = env.elasticsearch_client()
-    es.indices.create(index=es_index)
+    if not es.indices.exists(index=es_index):
+        es.indices.create(index=es_index)
 
 
 @pytest.fixture()
