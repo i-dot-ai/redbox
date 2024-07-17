d1174844deeb7866c70fed60721dea6a96a3527b
elliot.moore <elliotmoorecornwall@outlook.com>
2024-07-17 15:32:58 +0100

    Updated lambda code to latest

--- infrastructure/aws/cleanup_lambda/code/lambda.py
+++ infrastructure/aws/cleanup_lambda/code/lambda.py
@@ -3,8 +3,7 @@ import logging
 import os
 
 import pg8000.dbapi
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import scan
+from pg8000.native import literal
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -14,7 +13,7 @@ def lambda_handler(event, context):  # noqa: ARG001 unused args
     try:
         # Read FILE_EXPIRY_IN_SECONDS from environment
         logger.info("environment variable:  %s", os.environ["FILE_EXPIRY_IN_SECONDS"])
-        # FILE_EXPIRY_IN_SECONDS = os.environ["FILE_EXPIRY_IN_SECONDS"]
+        FILE_EXPIRY_IN_SECONDS = os.environ["FILE_EXPIRY_IN_SECONDS"]
 
         # Connect to pg database to find relevant files
         db_params = {
@@ -27,32 +26,26 @@ def lambda_handler(event, context):  # noqa: ARG001 unused args
             connection = pg8000.dbapi.Connection(**db_params)
             cursor = connection.cursor()
 
-            # For now to test the connection
-            cursor.execute("""SELECT id FROM redbox_core_user WHERE email='rachael.robinson@cabinetoffice.gov.uk'""")
-            for user_id in cursor.fetchall():
-                logger.info("user uuid:  %s", user_id)
+            cursor.execute(
+                # using parameters doesn't work for this statement; literal() used to reduce risk of SQL injection in f-string
+                f"""SELECT status
+                    FROM redbox_core_file
+                    WHERE redbox_core_file.last_referenced < (NOW() - INTERVAL '{literal(int(FILE_EXPIRY_IN_SECONDS))} seconds')
+                    AND redbox_core_file.status NOT IN ('deleted', 'errored'')
+                """
+            )
 
-                # Connect to elastic database to delete relevant files
-                client = Elasticsearch(cloud_id=os.environ["ELASTIC__CLOUD_ID"], api_key=os.environ["ELASTIC__API_KEY"])
-                results = scan(
-                    client=client,
-                    index="redbox-data-file",
-                    query={
-                        "query": {
-                            "bool": {
-                                "should": [
-                                    {"term": {"creator_user_uuid.keyword": str(user_id)}},
-                                    {"term": {"metadata.creator_user_uuid.keyword": str(user_id)}},
-                                ]
-                            }
-                        }
-                    },
-                    _source=False,
-                )
+            for file_id in cursor.fetchall():
+                logger.info("file uuid:  %s", file_id)
 
-                for r in results:
-                    logger.info("file uuid:  %s", r["_id"])
-                    # update pg database files as required
+                # # Connect to elastic database to delete relevant files
+                # client = Elasticsearch(cloud_id=os.environ["ELASTIC__CLOUD_ID"], api_key=os.environ["ELASTIC__API_KEY"])
+
+                # try:
+                #     resp = client.get(index="redbox-data-file", id=str(file_id))
+                #     logger.info("File: %s", resp)
+                # except NotFoundError as e:
+                #     logger.info("file/%s not found", file_id)
 
             connection.close()
             logger.info("Database connection closed.")
