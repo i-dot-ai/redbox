477ef2264ff51b0640654048a5783758f1d4a193
George Burton <g.e.c.cburton@gmail.com>
2024-07-18 15:49:34 +0100

    added real file and parametrized tests

--- worker/tests/test_app.py
+++ worker/tests/test_app.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from uuid import uuid4
+
 import pytest
 from faststream.redis import TestApp, TestRedisBroker
 from langchain_core.embeddings.fake import FakeEmbeddings
@@ -9,7 +12,14 @@ from worker import app as app_module
 
 
 @pytest.mark.asyncio()
-async def test_ingest_file(es_client, file: File, monkeypatch):
+@pytest.mark.parametrize(
+    "filename, status",
+    [
+        ("Cabinet Office - Wikipedia.pdf", ProcessingStatusEnum.complete),
+        ("Cabinet Office - Wikipedia.corrupt.pdf", ProcessingStatusEnum.failed),
+    ]
+)
+async def test_ingest_file(es_client, s3_client, monkeypatch, filename: str, status: ProcessingStatusEnum):
     """
     Given that I have written a text File to s3
     When I call ingest_file
@@ -17,6 +27,20 @@ async def test_ingest_file(es_client, file: File, monkeypatch):
     1. chunked
     2. written to Elasticsearch
     """
+
+    pdf = Path(__file__).parents[2] / "tests" / "data" / "pdf" / filename
+    file_name = pdf.name
+    file_type = pdf.suffix
+
+    with pdf.open("rb") as f:
+        s3_client.put_object(
+            Bucket=env.bucket_name,
+            Body=f.read(),
+            Key=file_name,
+            Tagging=f"file_type={file_type}",
+        )
+
+    file = File(key=file_name, bucket=env.bucket_name, creator_user_uuid=uuid4())
 
     storage_handler = ElasticsearchStorageHandler(es_client=es_client, root_index=env.elastic_root_index)
 
@@ -27,28 +51,11 @@ async def test_ingest_file(es_client, file: File, monkeypatch):
         await br.publish(file, list=env.ingest_queue_name)
         storage_handler.refresh()
 
-        chunks = storage_handler.get_file_chunks(file.uuid, file.creator_user_uuid)
-        assert len(chunks) > 0
+        if status == ProcessingStatusEnum.complete:
+            chunks = storage_handler.get_file_chunks(file.uuid, file.creator_user_uuid)
+            assert len(chunks) > 0
+        storage_handler.refresh()
+        file_status = storage_handler.get_file_status(file.uuid, file.creator_user_uuid)
+        assert file_status.processing_status == status, file_status.processing_status
 
 
-@pytest.mark.asyncio()
-async def test_ingest_file_fail(es_client, bad_file: File, monkeypatch):
-    """
-    Given that I have written a corrupt file to s3
-    When I call ingest_file
-    I Expect:
-     * 0 chunks to be produced
-     * file-status to be `failed`
-    """
-
-    storage_handler = ElasticsearchStorageHandler(es_client=es_client, root_index=env.elastic_root_index)
-
-    storage_handler.write_item(bad_file)
-
-    monkeypatch.setattr(app_module, "get_embeddings", lambda _: FakeEmbeddings(size=3072))
-    async with TestRedisBroker(broker) as br, TestApp(app):
-        await br.publish(bad_file, list=env.ingest_queue_name)
-
-    storage_handler.refresh()
-    status = storage_handler.get_file_status(bad_file.uuid, bad_file.creator_user_uuid)
-    assert status.processing_status == ProcessingStatusEnum.failed, status.processing_status
