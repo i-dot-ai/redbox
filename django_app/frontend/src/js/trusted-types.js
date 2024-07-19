31dad77819b143bcaec8f9950451544a826642ac
Kevin Etchells <kevetchells@hotmail.com>
2024-07-19 15:59:07 +0100

    Add copy-to-clipboard button

--- django_app/frontend/src/js/trusted-types.js
+++ django_app/frontend/src/js/trusted-types.js
@@ -15,7 +15,8 @@ if (typeof window.trustedTypes !== "undefined") {
             tagName === "sources-list" ||
             tagName === "tool-tip" ||
             tagName === "feedback-buttons" ||
-            tagName === "chat-title",
+            tagName === "chat-title" ||
+            tagName === "copy-text",
           attributeNameCheck: (attr) => true,
           allowCustomizedBuiltInElements: true,
         },
--- django_app/frontend/src/js/web-components/chats/sources-list.js
+++ django_app/frontend/src/js/web-components/chats/sources-list.js
@@ -42,14 +42,25 @@ class SourcesList extends HTMLElement {
     if (!chatId) {
       return;
     }
-    let link = document.createElement("a");
-    link.classList.add(
-      "iai-chat-bubble__citations-button",
-      "govuk-!-margin-left-2"
-    );
-    link.setAttribute("href", `/citations/${chatId}`);
-    link.textContent = "View information behind this answer";
-    this.querySelector(".iai-display-flex-from-desktop")?.appendChild(link);
+    if (this.sources.length) {
+      const html = `
+        <div class="iai-chat-bubble__citations-button-container">
+          <copy-text></copy-text>
+          <a class="iai-chat-bubble__citations-button" href="/citations/${chatId}">
+            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" focusable="false" aria-hidden="true">
+                <path d="M1.5 9.62502C1.5 9.62502 4.59036 3.55359 10 3.55359C15.4084 3.55359 18.5 9.62502 18.5 9.62502C18.5 9.62502 15.4084 15.6964 10 15.6964C4.59036 15.6964 1.5 9.62502 1.5 9.62502Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M9.99993 10.8392C10.322 10.8392 10.6308 10.7113 10.8586 10.4836C11.0863 10.2558 11.2142 9.94698 11.2142 9.62493C11.2142 9.30288 11.0863 8.99402 10.8586 8.7663C10.6308 8.53858 10.322 8.41064 9.99993 8.41064C9.67788 8.41064 9.36902 8.53858 9.1413 8.7663C8.91358 8.99402 8.78564 9.30288 8.78564 9.62493C8.78564 9.94698 8.91358 10.2558 9.1413 10.4836C9.36902 10.7113 9.67788 10.8392 9.99993 10.8392Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            See the response info
+          </a>
+        </div>
+      `;
+      /** @type {HTMLElement} */ (
+        this.querySelector(".iai-display-flex-from-desktop")
+      ).innerHTML += html;
+    } else {
+      this.innerHTML = `<copy-text></copy-text>`;
+    }
   };
 }
 customElements.define("sources-list", SourcesList);
