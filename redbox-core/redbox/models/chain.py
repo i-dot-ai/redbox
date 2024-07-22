24ca734b518a157d1a2b86c714b11c0f2ac757af
jamesrichards <james.richards@cabinetoffice.gov.uk>
2024-07-22 10:57:34 +0100

    Improved types in ChainInput

--- redbox-core/redbox/models/chain.py
+++ redbox-core/redbox/models/chain.py
@@ -5,17 +5,18 @@ to provide a pydantic v1 definition to work with these. As these models are most
 used in conjunction with langchain this is the tidiest boxing of pydantic v1 we can do
 """
 
-from typing import TypedDict
+from typing import TypedDict, Literal
+from uuid import UUID
 from langchain_core.pydantic_v1 import BaseModel, Field
 
 
 class ChainChatMessage(TypedDict):
-    role: str
+    role: Literal["user", "ai", "system"]
     text: str
 
 
 class ChainInput(BaseModel):
     question: str = Field(description="The last user chat message")
-    file_uuids: list[str] = Field(description="List of files to process")
-    user_uuid: str = Field(description="User the chain in executing for")
+    file_uuids: list[UUID] = Field(description="List of files to process")
+    user_uuid: UUID = Field(description="User the chain in executing for")
     chat_history: list[ChainChatMessage] = Field(description="All previous messages in chat (excluding question)")
