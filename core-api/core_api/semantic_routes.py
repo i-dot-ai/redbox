f960ab9927672db06436ce2bb651b45dce98d035
jamesrichards <james.richards@cabinetoffice.gov.uk>
2024-07-24 13:04:35 +0100

    Made routable chains runnables again to restore streaming

--- core-api/core_api/semantic_routes.py
+++ core-api/core_api/semantic_routes.py
@@ -35,30 +35,30 @@ def get_routable_chains(
     condense_chain: Annotated[Runnable, Depends(build_condense_retrieval_chain)],
     chat_chain: Annotated[Runnable, Depends(build_chat_chain)],
     chat_with_docs_chain: Annotated[Runnable, Depends(build_chat_with_docs_chain)],
-) -> dict[str, Tool]:
+) -> dict[str, tuple[Runnable, str]]:
     global __routable_chains  # noqa: PLW0603
     if not __routable_chains:
         chat_tools = (
-            as_chat_tool(
-                name=ChatRoute.info,
-                runnable=build_static_response_chain(INFO_RESPONSE, ChatRoute.info),
-                description="Give helpful information about Redbox",
+            (
+                ChatRoute.info,
+                build_static_response_chain(INFO_RESPONSE, ChatRoute.info),
+                "Give helpful information about Redbox",
             ),
-            as_chat_tool(
-                name=ChatRoute.chat,
-                runnable=chat_chain,
-                description="Answer questions as a helpful assistant",
+            (
+                ChatRoute.chat,
+                chat_chain,
+                "Answer questions as a helpful assistant",
             ),
-            as_chat_tool(
-                name=ChatRoute.chat_with_docs,
-                runnable=chat_with_docs_chain,
-                description="Answer questions as a helpful assistant using the documents provided",
+            (
+                ChatRoute.chat_with_docs,
+                chat_with_docs_chain,
+                "Answer questions as a helpful assistant using the documents provided",
             ),
-            as_chat_tool(
-                name=ChatRoute.search,
-                runnable=condense_chain,
-                description="Search for an answer to a question in provided documents",
+            (
+                ChatRoute.search,
+                condense_chain,
+                "Search for an answer to a question in provided documents",
             ),
         )
-        __routable_chains = {chat_tool.name: chat_tool for chat_tool in chat_tools}
+        __routable_chains = {name: (runnable,description) for (name,runnable,description) in chat_tools}
     return __routable_chains
