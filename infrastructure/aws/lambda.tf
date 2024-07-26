633aee39d309b04a503ec4effa578e174c13b405
elliot.moore <elliotmoorecornwall@outlook.com>
2024-07-26 09:29:13 +0100

    Removed comments and added lambda tag

--- infrastructure/aws/lambda.tf
+++ infrastructure/aws/lambda.tf
@@ -8,10 +8,10 @@ module "lambda-cleanup" {
     command = ["venv/bin/django-admin", each.value.command]
   }
   package_type = "Image"
-  image_uri              = "${var.ecr_repository_uri}/${var.project_name}-django-app:37d4ac8fe83c92653e79f2edc870747ffcc82d61"
+  image_uri              = "${var.ecr_repository_uri}/${var.project_name}-django-app:57733c2d0a87c689987fda5c5ca37a185e6c7a75"
   function_name          = "${local.name}-${each.value.task_name}-lambda"
   iam_role_name          = "${local.name}-${each.value.task_name}-lambda-role"
-  timeout                = 60
+  timeout                = 600
   environment_variables  = merge(local.django_app_secrets, local.django_lambda_environment_variables)
   aws_security_group_ids = [aws_security_group.service_security_group.id]
   subnet_ids             = data.terraform_remote_state.vpc.outputs.private_subnets
@@ -45,36 +45,3 @@ resource "aws_security_group_rule" "lambda_to_443_egress" {
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.service_security_group.id
 }
-
-#data "archive_file" "code" {
-#  type        = "zip"
-#  source_dir  = "${path.module}/cleanup_lambda/code"
-#  output_path = "${path.module}/cleanup_lambda/code.zip"
-#}
-#
-#
-#resource "aws_lambda_layer_version" "layer" {
-#  layer_name          = "cleanup-layer"
-#  filename            = data.archive_file.layer.output_path
-#  source_code_hash    = data.archive_file.layer.output_base64sha256
-#  compatible_runtimes = ["python3.12"]
-#}
-#
-#
-#data "archive_file" "layer" {
-#  type        = "zip"
-#  source_dir  = "${path.module}/cleanup_lambda/layer"
-#  output_path = "${path.module}/cleanup_lambda/layer.zip"
-#  depends_on  = [null_resource.pip_install]
-#}
-#
-#
-#resource "null_resource" "pip_install" {
-#  triggers = {
-#    shell_hash = sha256(file("${path.module}/cleanup_lambda/requirements.txt"))
-#  }
-#
-#  provisioner "local-exec" {
-#    command = "python3 -m pip install -r ./cleanup_lambda/requirements.txt -t ${path.module}/cleanup_lambda/layer/python"
-#  }
-#}
