009fc9827d9de3f375b8b018ccf5bc98f51ffd1d
George Burton <g.e.c.cburton@gmail.com>
2024-08-01 17:16:03 +0100

    remove comments

--- redbox-core/redbox/chains/components.py
+++ redbox-core/redbox/chains/components.py
@@ -66,22 +66,9 @@ def get_parameterised_retriever(env: Settings, embeddings: Embeddings | None = N
 
     Runnable returns a list of Chunks.
     """
-    # default_params = {
-    #     "size": env.ai.rag_k,
-    #     "num_candidates": env.ai.rag_num_candidates,
-    #     "match_boost": 1,
-    #     "knn_boost": 1,
-    #     "similarity_threshold": 0,
-    # }
     return ParameterisedElasticsearchRetriever(
         es_client=env.elasticsearch_client(),
         index_name=f"{env.elastic_root_index}-chunk",
-        # params=default_params,
         embedding_model=embeddings or get_embeddings(env),
         embedding_field_name=env.embedding_document_field_name,
     )
-    # .configurable_fields(
-    #     params=ConfigurableField(
-    #         id="params", name="Retriever parameters", description="A dictionary of parameters to use for the retriever."
-    #     )
-    # )
