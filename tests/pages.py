a802563ce13098db26dcb3cbfa7450a598523d3c
George Burton <g.e.c.cburton@gmail.com>
2024-08-02 08:54:29 +0100

    reduce diff

--- tests/pages.py
+++ tests/pages.py
@@ -258,15 +258,6 @@ class DocumentsPage(SignedInBasePage):
     def all_documents(self) -> list[DocumentRow]:
         return [DocumentRow.from_element(element) for element in self.page.locator(".iai-doc-list__item").all()]
 
-    @staticmethod
-    def _doc_from_element(element: Locator) -> DocumentRow:
-        filename = element.locator(".iai-doc-list__cell--file-name").inner_text()
-        status = element.locator(".iai-doc-list__cell--status").inner_text()
-        completed = element.evaluate(
-            "element => element.closest('.iai-doc-list').classList.contains('iai-doc-list--complete')"
-        )
-        return DocumentRow(filename=filename, status=status, completed=completed)
-
     def document_count(self) -> int:
         return len(self.all_documents)
 
