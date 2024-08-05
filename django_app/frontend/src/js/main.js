0ed6c66ac3421d1bbab2e82069447ede2981c602
Kevin Etchells <kevetchells@hotmail.com>
2024-08-05 13:01:42 +0100

    Prevent accidental double-clicking of submit buttons

--- django_app/frontend/src/js/main.js
+++ django_app/frontend/src/js/main.js
@@ -1,3 +1,18 @@
 import "./trusted-types.js";
 import "./libs/govuk-frontend.min.js";
 import "../../node_modules/i.ai-design-system/dist/iai-design-system.js";
+
+// Because this doesn't appear to be working in Redbox: https://design-system.service.gov.uk/components/button/#stop-users-from-accidentally-sending-information-more-than-once
+(() => {
+  let buttons = document.querySelectorAll(`[data-prevent-double-click="true"]`);
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      window.setTimeout(() => {
+        button.disabled = true;
+        window.setTimeout(() => {
+          button.disabled = false;
+        }, 1000);
+      }, 1);
+    });
+  });
+})();
