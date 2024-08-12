1034d28e7b293ecfaf9a45ce2e3e9f5215281e22
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2024-08-12 16:01:56 +0100

    comment out streamlit url as view delete :(

--- django_app/redbox_app/urls.py
+++ django_app/redbox_app/urls.py
@@ -39,9 +39,13 @@ file_urlpatterns = [
 chat_urlpatterns = [
     path("chats/<uuid:chat_id>/", views.ChatsView.as_view(), name="chats"),
     path("chats/", views.ChatsView.as_view(), name="chats"),
-    path("chat/<uuid:chat_id>/title/", views.ChatsTitleView.as_view(), name="chat-titles"),
+    path(
+        "chat/<uuid:chat_id>/title/", views.ChatsTitleView.as_view(), name="chat-titles"
+    ),
     path("post-message/", views.post_message, name="post-message"),
-    path("citations/<uuid:message_id>/", views.CitationsView.as_view(), name="citations"),
+    path(
+        "citations/<uuid:message_id>/", views.CitationsView.as_view(), name="citations"
+    ),
     path("ratings/<uuid:message_id>/", views.RatingsView.as_view(), name="ratings"),
 ]
 
@@ -54,14 +58,25 @@ other_urlpatterns = [
     path("", views.homepage_view, name="homepage"),
     path("health/", views.health, name="health"),
     path("file-status/", views.file_status_api_view, name="file-status"),
-    path("check-demographics/", views.CheckDemographicsView.as_view(), name="check-demographics"),
+    path(
+        "check-demographics/",
+        views.CheckDemographicsView.as_view(),
+        name="check-demographics",
+    ),
     path("demographics/", views.DemographicsView.as_view(), name="demographics"),
-    path("streamlit/", views.streamlit_view, name="streamlit_view"),
+    # path("streamlit/", views.streamlit_view, name="streamlit_view"),
 ]
 
 urlpatterns = (
-    info_urlpatterns + other_urlpatterns + auth_urlpatterns + chat_urlpatterns + file_urlpatterns + admin_urlpatterns
+    info_urlpatterns
+    + other_urlpatterns
+    + auth_urlpatterns
+    + chat_urlpatterns
+    + file_urlpatterns
+    + admin_urlpatterns
 )
 
 if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
