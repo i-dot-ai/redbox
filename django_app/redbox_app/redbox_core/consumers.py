2f7c589a0fa383093a6eaf2bb8ea06c0f6dd98c0
George Burton <8233643+gecBurton@users.noreply.github.com>
2024-08-12 11:16:48 +0100

    Feature/simplify consumers (#947)
    * remove file_save
    
    * removed get_messages
    
    * removed get_files_by_id
    
    * removed get_session
    
    * removed get_sources_with_files
    
    * removed save_message
    
    * tidy up
    
    * tidy up
    
    * simplified handle_documents and added a docstring
    
    ---------
    
    Co-authored-by: George Burton <g.e.c.cburton@gmail.com>

--- django_app/redbox_app/redbox_core/consumers.py
+++ django_app/redbox_app/redbox_core/consumers.py
@@ -53,42 +53,70 @@ class ChatConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data or bytes_data)
         logger.debug("received %s from browser", data)
         user_message_text: str = data.get("message", "")
-        session_id: str | None = data.get("sessionId", None)
+
         selected_file_uuids: Sequence[UUID] = [UUID(u) for u in data.get("selectedFiles", [])]
         user: User = self.scope.get("user", None)
 
-        session: Chat = await self.get_session(session_id, user, user_message_text)
+        if session_id := data.get("sessionId"):
+            chat = await Chat.objects.aget(id=session_id)
+        else:
+            chat = await Chat.objects.acreate(name=user_message_text[0 : settings.CHAT_TITLE_LENGTH], user=user)
 
         # save user message
-        selected_files = await self.get_files_by_id(selected_file_uuids, user)
-        await self.save_message(session, user_message_text, ChatRoleEnum.user, selected_files=selected_files)
+        message = await ChatMessage.objects.acreate(chat=chat, text=user_message_text, role=ChatRoleEnum.user)
+        async for file in File.objects.filter(id__in=selected_file_uuids, user=user):
+            message.selected_files.add(file)
 
-        await self.llm_conversation(selected_files, session, user, user_message_text)
+        await self.llm_conversation(message, user)
         await self.close()
 
-    async def llm_conversation(self, selected_files: Sequence[File], session: Chat, user: User, title: str) -> None:
+    async def llm_conversation(self, chat_message: ChatMessage, user: User) -> None:
         """Initiate & close websocket conversation with the core-api message endpoint."""
-        session_messages = await self.get_messages(session)
+
         message_history: Sequence[Mapping[str, str]] = [
-            {"role": message.role, "text": message.text} for message in session_messages
+            {"role": message.role, "text": message.text}
+            async for message in ChatMessage.objects.filter(chat=chat_message.chat).order_by("created_at")
         ]
         url = URL.build(scheme="ws", host=settings.CORE_API_HOST, port=settings.CORE_API_PORT) / "chat/rag"
         try:
             async with connect(str(url), extra_headers={"Authorization": user.get_bearer_token()}) as core_websocket:
                 message = {
                     "message_history": message_history,
-                    "selected_files": [{"uuid": f.core_file_uuid} for f in selected_files],
+                    "selected_files": [{"uuid": f.core_file_uuid} async for f in chat_message.selected_files.all()],
                     "ai_settings": await self.get_ai_settings(user),
                 }
                 await self.send_to_server(core_websocket, message)
-                await self.send_to_client("session-id", session.id)
-                reply, citations, route = await self.receive_llm_responses(user, core_websocket)
-            message = await self.save_message(session, reply, ChatRoleEnum.ai, sources=citations, route=route)
-            await self.send_to_client("end", {"message_id": message.id, "title": title, "session_id": session.id})
+                await self.send_to_client("session-id", chat_message.chat.id)
+                reply, files_and_citations, route = await self.receive_llm_responses(user, core_websocket)
+                for file, _ in files_and_citations:
+                    await self.send_to_client(
+                        "source", {"url": str(file.url), "original_file_name": file.original_file_name}
+                    )
 
-            for file, _ in citations:
+            chat_message = await ChatMessage.objects.acreate(
+                chat=chat_message.chat, text=reply, role=ChatRoleEnum.ai, route=route
+            )
+
+            for file, citations in files_and_citations:
                 file.last_referenced = timezone.now()
-                await self.file_save(file)
+                await file.asave()
+                for citation in citations:
+                    await Citation.objects.acreate(
+                        chat_message=chat_message,
+                        file=file,
+                        text=citation.page_content,
+                        page_numbers=citation.page_numbers,
+                    )
+
+            await self.send_to_client(
+                "end",
+                {
+                    "message_id": chat_message.id,
+                    "title": chat_message.text,
+                    "session_id": chat_message.chat.id,
+                },
+            )
+
         except (TimeoutError, ConnectionClosedError, CancelledError, CoreError) as e:
             logger.exception("Error from core.", exc_info=e)
             await self.send_to_client("error", error_messages.CORE_ERROR_MESSAGE)
@@ -116,10 +144,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def handle_documents(
         self, response: CoreChatResponse, user: User
     ) -> Sequence[tuple[File, CoreChatResponseDoc]]:
-        source_files, citations = await self.get_sources_with_files(response.data, user)
-        for file in source_files:
-            await self.send_to_client("source", {"url": str(file.url), "original_file_name": file.original_file_name})
-        return citations
+        """this function enriches the citations received from the core-api with
+        the matching djangio File objects."""
+        docs = response.data
+
+        return [
+            (file, [doc for doc in docs if doc.file_uuid == file.core_file_uuid])
+            async for file in File.objects.filter(core_file_uuid__in=[doc.file_uuid for doc in docs], user=user)
+        ]
 
     async def handle_text(self, response: CoreChatResponse) -> str:
         await self.send_to_client("text", response.data)
@@ -157,67 +189,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def send_to_server(websocket: WebSocketClientProtocol, data: Mapping[str, Any]) -> None:
         logger.debug("sending %s to core-api", data)
         return await websocket.send(json.dumps(data, default=str))
-
-    @staticmethod
-    @database_sync_to_async
-    def get_session(session_id: str, user: User, user_message_text: str) -> Chat:
-        if session_id:
-            session = Chat.objects.get(id=session_id)
-        else:
-            session_name = user_message_text[0 : settings.CHAT_TITLE_LENGTH]
-            session = Chat(name=session_name, user=user)
-            session.save()
-        return session
-
-    @staticmethod
-    @database_sync_to_async
-    def get_messages(session: Chat) -> Sequence[ChatMessage]:
-        return list(ChatMessage.objects.filter(chat=session).order_by("created_at"))
-
-    @staticmethod
-    @database_sync_to_async
-    def save_message(
-        session: Chat,
-        user_message_text: str,
-        role: ChatRoleEnum,
-        sources: Sequence[tuple[File, CoreChatResponseDoc]] | None = None,
-        selected_files: Sequence[File] | None = None,
-        route: str | None = None,
-    ) -> ChatMessage:
-        chat_message = ChatMessage(chat=session, text=user_message_text, role=role, route=route)
-        chat_message.save()
-        if sources:
-            for file, citations in sources:
-                for citation in citations:
-                    Citation.objects.create(
-                        chat_message=chat_message,
-                        file=file,
-                        text=citation.page_content,
-                        page_numbers=citation.page_numbers,
-                    )
-        if selected_files:
-            chat_message.selected_files.set(selected_files)
-        return chat_message
-
-    @staticmethod
-    @database_sync_to_async
-    def get_files_by_id(docs: Sequence[UUID], user: User) -> Sequence[File]:
-        return list(File.objects.filter(id__in=docs, user=user))
-
-    @staticmethod
-    @database_sync_to_async
-    def get_sources_with_files(
-        docs: Sequence[CoreChatResponseDoc], user: User
-    ) -> tuple[Sequence[File], Sequence[tuple[File, Sequence[CoreChatResponseDoc]]]]:
-        uuids = [doc.file_uuid for doc in docs]
-        files = File.objects.filter(core_file_uuid__in=uuids, user=user)
-
-        return files, [(file, [doc for doc in docs if doc.file_uuid == file.core_file_uuid]) for file in files]
-
-    @staticmethod
-    @database_sync_to_async
-    def file_save(file):
-        return file.save()
 
     @staticmethod
     @database_sync_to_async
