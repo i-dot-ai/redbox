15cc29d1d703bf0c30f0ab2d08030b21f1668422
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2024-08-14 08:05:10 +0100

    add back sentence transformers embeddings

--- redbox-core/redbox/embeddings/__init__.py
+++ redbox-core/redbox/embeddings/__init__.py
@@ -1,7 +1,8 @@
 from langchain_core.embeddings import Embeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from redbox.models.settings import Settings
 
 from .langchain import get_azure_embeddings, get_openai_embeddings
-from redbox.models.settings import Settings
 
 
 def get_embeddings(env: Settings) -> Embeddings:
@@ -9,5 +10,9 @@ def get_embeddings(env: Settings) -> Embeddings:
         return get_azure_embeddings(env)
     elif env.embedding_backend == "openai":
         return get_openai_embeddings(env)
+    elif env.embedding_backend == "sentencetransformers":
+        return HuggingFaceEmbeddings(
+            model_name=env.embedding_openai_model,
+        )
     else:
         raise Exception("No configured embedding model")
