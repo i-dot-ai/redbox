68be33cda414e204e41e374dfa04043134191410
George Burton <8233643+gecBurton@users.noreply.github.com>
2024-08-23 17:33:41 +0100

    feature/remove redis (#988)
    * remove redis
    
    * remove some more env vars
    
    * put back StatusResponse
    
    ---------
    
    Co-authored-by: George Burton <g.e.c.cburton@gmail.com>

--- redbox-core/redbox/models/__init__.py
+++ redbox-core/redbox/models/__init__.py
@@ -1,10 +1,4 @@
 from redbox.models.chat import ChatMessage, ChatRequest, ChatResponse, ChatRoute
-from redbox.models.embedding import (
-    EmbeddingModelInfo,
-    EmbeddingResponse,
-    EmbedQueueItem,
-    StatusResponse,
-)
 from redbox.models.errors import (
     APIError404,
     APIErrorDetail,
@@ -17,6 +11,7 @@ from redbox.models.file import (
 )
 from redbox.models.persona import ChatPersona
 from redbox.models.settings import Settings
+from redbox.models.status import StatusResponse
 
 __all__ = [
     "APIError404",
@@ -27,9 +22,6 @@ __all__ = [
     "ChatRequest",
     "ChatResponse",
     "ChatRoute",
-    "EmbedQueueItem",
-    "EmbeddingModelInfo",
-    "EmbeddingResponse",
     "File",
     "FileStatus",
     "ProcessingStatusEnum",
