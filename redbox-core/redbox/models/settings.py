96d65a9c7aeeca08eaf1a1fcabdfea02ccb9aba6
jamesrichards <james.richards@cabinetoffice.gov.uk>
2024-08-23 11:06:13 +0100

    LLM fallbacks implemented across regions with a new env var

--- redbox-core/redbox/models/settings.py
+++ redbox-core/redbox/models/settings.py
@@ -41,16 +41,19 @@ class Settings(BaseSettings):
     openai_api_version_35t: str = "2023-12-01-preview"
     azure_openai_api_key_35t: str = "not a key"
     azure_openai_endpoint_35t: str = "not an endpoint"
+    azure_openai_fallback_endpoint_35t: str | None = None
 
     # azure/gpt-4
     openai_api_version_4t: str = "2024-02-01"
     azure_openai_api_key_4t: str = "not a key"
     azure_openai_endpoint_4t: str = "not an endpoint"
+    azure_openai_fallback_endpoint_4t: str | None = None
 
     # azure/gpt-4o
     openai_api_version_4o: str = "2024-02-01"
     azure_openai_api_key_4o: str = "not a key"
     azure_openai_endpoint_4o: str = "not an endpoint"
+    azure_openai_fallback_endpoint_4o: str | None = None
 
     embedding_openai_api_key: str = "NotAKey"
     embedding_azure_openai_endpoint: str = "not an endpoint"
