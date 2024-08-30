f8c0b6d46985b1c195bce1f926d99746052e3203
George Burton <g.e.c.cburton@gmail.com>
2024-08-30 13:04:40 +0100

    set workers to 1

--- django_app/redbox_app/settings.py
+++ django_app/redbox_app/settings.py
@@ -342,6 +342,7 @@ Q_CLUSTER = {
     "max_attempts": env.int("Q_MAX_ATTEMPTS", 1),
     "catch_up": False,
     "orm": "default",
+    "workers": 1,
 }
 
 UNSTRUCTURED_HOST = env.str("UNSTRUCTURED_HOST")
