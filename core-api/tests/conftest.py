5af23c0cbc524f8091c5e3bf82552972448d8cd1
George Burton <g.e.c.cburton@gmail.com>
2024-09-03 08:43:20 +0100

    update core-api

--- core-api/tests/conftest.py
+++ core-api/tests/conftest.py
@@ -10,7 +10,7 @@ from jose import jwt
 from langchain_core.documents.base import Document
 from langchain_core.embeddings.fake import FakeEmbeddings
 from langchain_elasticsearch.vectorstores import ElasticsearchStore
-from redbox.models import File, Settings
+from redbox.models import Settings
 from redbox.models.file import ChunkMetadata, ChunkResolution
 
 from core_api import dependencies
@@ -109,23 +109,21 @@ def file_pdf_path() -> Path:
 
 
 @pytest.fixture()
-def file_pdf_object(file_pdf_path: Path, alice: UUID, env: Settings) -> File:
+def file_pdf(file_pdf_path: Path, alice: UUID, env: Settings) -> str:
     """The unuploaded File object of Alice's PDF."""
     file_name = file_pdf_path.name
-    return File(key=file_name, bucket=env.bucket_name, creator_user_uuid=alice)
+    return file_name
 
 
 @pytest.fixture()
-def file_pdf_chunks(file_pdf_object: File) -> list[Document]:
+def file_pdf_chunks(file_pdf) -> list[Document]:
     """The Document chunk objects of Alice's PDF."""
     normal_chunks = [
         Document(
             page_content="hello",
             metadata=ChunkMetadata(
-                parent_file_uuid=str(file_pdf_object.uuid),
                 index=i,
-                file_name=file_pdf_object.key,
-                creator_user_uuid=file_pdf_object.creator_user_uuid,
+                file_name=file_pdf,
                 page_number=4,
                 created_datetime=datetime.now(UTC),
                 token_count=4,
@@ -139,10 +137,8 @@ def file_pdf_chunks(file_pdf_object: File) -> list[Document]:
         Document(
             page_content="hello" * 10,
             metadata=ChunkMetadata(
-                parent_file_uuid=str(file_pdf_object.uuid),
                 index=i,
-                file_name=file_pdf_object.key,
-                creator_user_uuid=file_pdf_object.creator_user_uuid,
+                file_name=file_pdf,
                 page_number=4,
                 created_datetime=datetime.now(UTC),
                 token_count=20,
@@ -161,23 +157,21 @@ def file_html_path() -> Path:
 
 
 @pytest.fixture()
-def file_html_object(file_html_path: Path, alice: UUID, env: Settings) -> File:
+def file_html(file_html_path: Path, alice: UUID, env: Settings) -> str:
     """The unuploaded File object of Alice's HTML."""
     file_name = file_html_path.name
-    return File(key=file_name, bucket=env.bucket_name, creator_user_uuid=alice)
+    return file_name
 
 
 @pytest.fixture()
-def file_html_chunks(file_html_object: File) -> list[Document]:
+def file_html_chunks(file_html: str) -> list[Document]:
     """The Document chunk objects of Alice's HTML."""
     normal_chunks = [
         Document(
             page_content="hello",
             metadata=ChunkMetadata(
-                parent_file_uuid=str(file_html_object.uuid),
                 index=i,
-                file_name=file_html_object.key,
-                creator_user_uuid=file_html_object.creator_user_uuid,
+                file_name=file_html,
                 page_number=4,
                 created_datetime=datetime.now(UTC),
                 token_count=4,
@@ -191,10 +185,8 @@ def file_html_chunks(file_html_object: File) -> list[Document]:
         Document(
             page_content="hello" * 10,
             metadata=ChunkMetadata(
-                parent_file_uuid=str(file_html_object.uuid),
                 index=i,
-                file_name=file_html_object.key,
-                creator_user_uuid=file_html_object.creator_user_uuid,
+                file_name=file_html,
                 page_number=4,
                 created_datetime=datetime.now(UTC),
                 token_count=20,
