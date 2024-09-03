87ee24131ecfa6e88c33e1b89cad2f47e2f0fb48
George Burton <g.e.c.cburton@gmail.com>
2024-09-03 16:13:36 +0100

    revert infra changes

--- infrastructure/aws/rds.tf
+++ infrastructure/aws/rds.tf
@@ -1,7 +1,7 @@
 module "rds" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
-  source = "../../../i-dot-ai-core-terraform-modules//modules/infrastructure/rds"  # For testing local changes
-#   source                  = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/rds?ref=v1.0.0-rds"
+  # source = "../../../i-dot-ai-core-terraform-modules//modules/infrastructure/rds"  # For testing local changes
+  source                  = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/rds?ref=v1.0.0-rds"
   name                    = local.name
   db_name                 = var.project_name
   domain_name             = var.domain_name
