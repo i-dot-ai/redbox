0b8414534c9769a679083bdc1a707fdf8c213e71
George Burton <g.e.c.cburton@gmail.com>
2024-09-04 12:11:32 +0100

    remove all references to CORE_API_HOST

--- django_app/tests/management/test_commands.py
+++ django_app/tests/management/test_commands.py
@@ -1,16 +1,11 @@
-import re
-import uuid
 from datetime import UTC, datetime, timedelta
-from http import HTTPStatus
 from io import StringIO
 from unittest.mock import MagicMock, patch
 
 import elasticsearch
 import pytest
-import requests
 from botocore.exceptions import UnknownClientMethodError
 from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management import CommandError, call_command
 from django.utils import timezone
 from freezegun import freeze_time
@@ -20,68 +15,6 @@ from requests_mock import Mocker
 from redbox_app.redbox_core.models import Chat, ChatMessage, ChatRoleEnum, File, StatusEnum, User
 
 # === check_file_status command tests ===
-
-
-@patch("redbox_app.redbox_core.models.File.delete_from_s3")
-@patch("redbox_app.redbox_core.models.File.original_file.field.storage.save")
-@pytest.mark.django_db()
-def test_check_file_status(deletion_mock: MagicMock, put_mock: MagicMock, alice: User, requests_mock: Mocker):
-    # Based on: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/delete_object.html
-    deletion_mock.side_effect = {
-        "DeleteMarker": True,
-    }
-    put_mock.side_effect = yield
-
-    # Given
-    def create_files():
-        files = []
-
-        for i in range(3):
-            filename = f"original_file_{i}.txt"
-            files.append(
-                File.objects.create(
-                    user=alice,
-                    original_file=SimpleUploadedFile(filename, b"Lorem Ipsum."),
-                    original_file_name=filename,
-                    core_file_uuid=uuid.uuid4(),
-                )
-            )
-        return files
-
-    file_in_core_api, file_with_surprising_status, file_not_in_core_api, file_core_api_error = create_files()
-
-    matcher = re.compile(f"http://{settings.CORE_API_HOST}:{settings.CORE_API_PORT}/file/[0-9a-f]|\\-/status")
-
-    requests_mock.get(
-        matcher,
-        status_code=HTTPStatus.CREATED,
-        json={
-            "processing_status": StatusEnum.processing,
-        },
-    )
-    requests_mock.get(
-        f"http://{settings.CORE_API_HOST}:{settings.CORE_API_PORT}/file/{file_with_surprising_status.core_file_uuid}/status",
-        status_code=HTTPStatus.CREATED,
-        json={
-            "processing_status": "this_is_a_surprising_string",
-        },
-    )
-    requests_mock.get(
-        f"http://{settings.CORE_API_HOST}:{settings.CORE_API_PORT}/file/{file_not_in_core_api.core_file_uuid}/status",
-        status_code=HTTPStatus.NOT_FOUND,
-    )
-    requests_mock.get(
-        f"http://{settings.CORE_API_HOST}:{settings.CORE_API_PORT}/file/{file_core_api_error.core_file_uuid}/status",
-        exc=requests.exceptions.Timeout,
-    )
-    # When
-    call_command("check_file_status")
-
-    # Then
-    assert File.objects.get(id=file_in_core_api.id).status == StatusEnum.processing
-    assert File.objects.get(id=file_with_surprising_status.id).status == StatusEnum.processing
-    assert File.objects.get(id=file_not_in_core_api.id).status == StatusEnum.deleted
-    assert File.objects.get(id=file_core_api_error.id).status == StatusEnum.errored
 
 
 # === show_magiclink_url command tests ===
@@ -149,24 +82,11 @@ EXPIRED_FILE_DATE = timezone.now() - timedelta(seconds=(settings.FILE_EXPIRY_IN_
     ],
 )
 @pytest.mark.django_db()
-def test_delete_expired_files(
-    uploaded_file: File, requests_mock: Mocker, last_referenced: datetime, should_delete: bool
-):
+def test_delete_expired_files(uploaded_file: File, last_referenced: datetime, should_delete: bool):
     # Given
     mock_file = uploaded_file
     mock_file.last_referenced = last_referenced
     mock_file.save()
-
-    matcher = re.compile(f"http://{settings.CORE_API_HOST}:{settings.CORE_API_PORT}/file/[0-9a-f]|\\-")
-    requests_mock.delete(
-        matcher,
-        status_code=HTTPStatus.CREATED,
-        json={
-            "key": mock_file.original_file_name,
-            "bucket": settings.BUCKET_NAME,
-            "uuid": str(uuid.uuid4()),
-        },
-    )
 
     # When
     call_command("delete_expired_data")
