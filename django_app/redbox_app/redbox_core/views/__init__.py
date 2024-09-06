126fd1bd99dbfadaa122595ee5d055be8e4a326c
Kevin Etchells <kevetchells@hotmail.com>
2024-09-06 16:41:46 +0100

    Remove redirect to demographics page

--- django_app/redbox_app/redbox_core/views/__init__.py
+++ django_app/redbox_app/redbox_core/views/__init__.py
@@ -2,7 +2,7 @@ from redbox_app.redbox_core.dash_apps import report_app
 from redbox_app.redbox_core.views.auth_views import sign_in_link_sent_view, sign_in_view, signed_out_view
 from redbox_app.redbox_core.views.chat_views import ChatsTitleView, ChatsView, UpdateChatFeedback
 from redbox_app.redbox_core.views.citation_views import CitationsView
-from redbox_app.redbox_core.views.demographics_views import CheckDemographicsView, DemographicsView, UpdateDemographicsView
+from redbox_app.redbox_core.views.demographics_views import DemographicsView, UpdateDemographicsView
 from redbox_app.redbox_core.views.document_views import (
     DocumentView,
     UploadView,
@@ -16,13 +16,13 @@ from redbox_app.redbox_core.views.ratings_views import RatingsView
 __all__ = [
     "ChatsTitleView",
     "ChatsView",
-    "CheckDemographicsView",
     "CitationsView",
     "DemographicsView",
     "DocumentView",
     "RatingsView",
     "SecurityTxtRedirectView",
     "UploadView",
+    "UpdateDemographicsView",
     "file_status_api_view",
     "health",
     "homepage_view",
