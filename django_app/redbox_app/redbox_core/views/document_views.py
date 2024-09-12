28b5fbf26c67905ee746b5a6d677a7258c8ffd45
George Burton <8233643+gecBurton@users.noreply.github.com>
2024-09-12 08:35:31 +0100

    feature/added images to file types (#1041)
    * added images to file types
    
    * removed fast strategy
    
    ---------
    
    Co-authored-by: George Burton <g.e.c.cburton@gmail.com>

--- django_app/redbox_app/redbox_core/views/document_views.py
+++ django_app/redbox_app/redbox_core/views/document_views.py
@@ -45,6 +45,10 @@ APPROVED_FILE_EXTENSIONS = [
     ".tsv",
     ".xlsx",
     ".htm",
+    ".bmp",
+    ".jpeg",
+    ".png",
+    ".jpeg",
 ]
 MAX_FILE_SIZE = 209715200  # 200 MB or 200 * 1024 * 1024
 
