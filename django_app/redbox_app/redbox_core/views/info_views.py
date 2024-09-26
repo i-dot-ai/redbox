1acdb8e788e104037dcff2bc3ce528973e8872ee
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2024-09-26 12:45:27 +0100

    adjustments

--- django_app/redbox_app/redbox_core/views/info_views.py
+++ django_app/redbox_app/redbox_core/views/info_views.py
@@ -10,11 +10,15 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET"])
 def privacy_notice_view(request):
+    if settings.REPO_OWNER.lower() == "uktrade":
+        return render(request, "privacy-notice-uktrade.html", {})
     return render(request, "privacy-notice.html", {})
 
 
 @require_http_methods(["GET"])
 def support_view(request):
+    if settings.REPO_OWNER.lower() == "uktrade":
+        return render(request, "support-uktrade.html", {"contact_email": settings.CONTACT_EMAIL})
     return render(
         request, "support.html", {"contact_email": settings.CONTACT_EMAIL, "version": settings.REDBOX_VERSION}
     )
@@ -22,4 +26,6 @@ def support_view(request):
 
 @require_http_methods(["GET"])
 def accessibility_statement_view(request):
+    if settings.REPO_OWNER.lower() == "uktrade":
+        return render(request, "accessibility-statement-uktrade.html", {"contact_email": settings.CONTACT_EMAIL})
     return render(request, "accessibility-statement.html", {"contact_email": settings.CONTACT_EMAIL})
