08f43fb000b192572383d6da116e306f56442294
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2024-10-03 14:28:59 +0100

    waffle integration, removing conflicting settings changes and usage of repo owner env var

--- django_app/redbox_app/redbox_core/views/info_views.py
+++ django_app/redbox_app/redbox_core/views/info_views.py
@@ -6,18 +6,19 @@ These shouldn't contain sensitive data and don't require login.
 from django.conf import settings
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+import waffle
 
 
 @require_http_methods(["GET"])
 def privacy_notice_view(request):
-    if settings.REPO_OWNER.lower() == "uktrade":
+    if waffle.flag_is_active(request, 'uktrade'):
         return render(request, "privacy-notice-uktrade.html", {})
     return render(request, "privacy-notice.html", {})
 
 
 @require_http_methods(["GET"])
 def support_view(request):
-    if settings.REPO_OWNER.lower() == "uktrade":
+    if waffle.flag_is_active(request, 'uktrade'):
         return render(request, "support-uktrade.html", {"contact_email": settings.CONTACT_EMAIL})
     return render(
         request, "support.html", {"contact_email": settings.CONTACT_EMAIL, "version": settings.REDBOX_VERSION}
@@ -26,6 +27,6 @@ def support_view(request):
 
 @require_http_methods(["GET"])
 def accessibility_statement_view(request):
-    if settings.REPO_OWNER.lower() == "uktrade":
+    if waffle.flag_is_active(request, 'uktrade'):
         return render(request, "accessibility-statement-uktrade.html", {"contact_email": settings.CONTACT_EMAIL})
     return render(request, "accessibility-statement.html", {"contact_email": settings.CONTACT_EMAIL})
