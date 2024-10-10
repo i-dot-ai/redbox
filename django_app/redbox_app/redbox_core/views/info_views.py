f6e639663ebbd6a5e19295b35e5cb3349538a433
gecBurton <g.e.c.burton@gmail.com>
2024-10-10 16:29:03 +0100

    fixed formatting

--- django_app/redbox_app/redbox_core/views/info_views.py
+++ django_app/redbox_app/redbox_core/views/info_views.py
@@ -3,15 +3,21 @@ Views for info pages like privacy notice, accessibility statement, etc.
 These shouldn't contain sensitive data and don't require login.
 """
 
+import waffle
 from django.conf import settings
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-import waffle
 
 
 @require_http_methods(["GET"])
 def privacy_notice_view(request):
-    return render(request, "privacy-notice.html", {"waffle_flag": waffle.flag_is_active,})
+    return render(
+        request,
+        "privacy-notice.html",
+        {
+            "waffle_flag": waffle.flag_is_active,
+        },
+    )
 
 
 @require_http_methods(["GET"])
@@ -23,4 +29,11 @@ def support_view(request):
 
 @require_http_methods(["GET"])
 def accessibility_statement_view(request):
-    return render(request, "accessibility-statement.html", {"contact_email": settings.CONTACT_EMAIL, "waffle_flag": waffle.flag_is_active,})
+    return render(
+        request,
+        "accessibility-statement.html",
+        {
+            "contact_email": settings.CONTACT_EMAIL,
+            "waffle_flag": waffle.flag_is_active,
+        },
+    )
