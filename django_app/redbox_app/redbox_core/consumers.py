4c917f70056abf5a233d8a84a681e8c3e5bdd8c2
gecBurton <g.e.c.burton@gmail.com>
2024-10-11 08:05:21 +0100

    rationaised session update

--- django_app/redbox_app/redbox_core/consumers.py
+++ django_app/redbox_app/redbox_core/consumers.py
@@ -27,6 +27,9 @@ from redbox.models.chain import (
 )
 from redbox_app.redbox_core import error_messages
 from redbox_app.redbox_core.models import (
+    AISettings as AISettingsModel,
+)
+from redbox_app.redbox_core.models import (
     Chat,
     ChatLLMBackend,
     ChatMessage,
@@ -78,23 +81,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
         selected_file_uuids: Sequence[UUID] = [UUID(u) for u in data.get("selectedFiles", [])]
         user: User = self.scope.get("user")
 
+        user_ai_settings = await AISettingsModel.objects.aget(label=user.ai_settings_id)
+
+        if "llm" in data:
+            chat_backend = await ChatLLMBackend.objects.aget(name=data.get("llm"))
+        else:
+            chat_backend = await ChatLLMBackend.objects.aget(id=user_ai_settings.chat_backend_id)
+
+        temperature = data.get("temperature", user_ai_settings.temperature)
+
         if session_id := data.get("sessionId"):
             session = await Chat.objects.aget(id=session_id)
-
-            if llm := data.get("llm"):
-                chat_backend = await ChatLLMBackend.objects.aget(name=llm)
-                logger.info("updating to: chat_backend=%s", chat_backend)
-                session.chat_backend = chat_backend
-            else:
-                logger.info("llm not specified, just have %s", data)
-
-            if "temperature" in data:
-                session.temperature = data.get("temperature")
-
+            session.chat_backend = chat_backend
+            session.temperature = temperature
+            logger.info("updating session: chat_backend=%s temperature=%s", chat_backend, temperature)
             await session.asave()
         else:
-            logger.debug("creating new Chat session")
-            session = await Chat.objects.acreate(name=user_message_text[: settings.CHAT_TITLE_LENGTH], user=user)
+            logger.info("creating session: chat_backend=%s temperature=%s", chat_backend, temperature)
+            session = await Chat.objects.acreate(
+                name=user_message_text[: settings.CHAT_TITLE_LENGTH],
+                user=user,
+                chat_backend=chat_backend,
+                temperature=temperature,
+            )
 
         # save user message
         permitted_files = File.objects.filter(user=user, status=StatusEnum.complete)
