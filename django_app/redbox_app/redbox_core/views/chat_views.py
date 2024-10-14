5d571d0756fd07b4eaaa5daca4515f8ad2c52b29
Rachael Robinson <23265724+rachaelcodes@users.noreply.github.com>
2024-10-14 12:59:52 +0100

    account for docker ports

--- django_app/redbox_app/redbox_core/views/chat_views.py
+++ django_app/redbox_app/redbox_core/views/chat_views.py
@@ -35,7 +35,7 @@ class ChatsView(View):
             messages = ChatMessage.get_messages_ordered_by_citation_priority(chat_id)
         endpoint = URL.build(
             scheme=settings.WEBSOCKET_SCHEME,
-            host=request.META["SERVER_NAME"],
+            host="localhost" if settings.ENVIRONMENT.is_test else request.META["SERVER_NAME"],
             port=int(request.META["SERVER_PORT"]),
             path=r"/ws/chat/",
         )
