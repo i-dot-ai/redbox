0539bb8d7248674eafbfc91ea9952dcb6f2fe236
Morgan Frodsham <morgan.frodsham@cabinetoffice.gov.uk>
2024-10-15 11:38:31 +0100

    Updated python format

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -10,9 +10,10 @@ import asyncio
 # Load Redbox environment variables
 load_dotenv("../.env")
 
+
 # Function defining how the Redbox streamlit app will work
 def run_streamlit():
-    # Create two tabs within the interface, one labeled "chat", the othe rlabeled "settings"
+    # Create two tabs within the interface, one labeled "chat", the other labeled "settings"
     chat_tab, settings_tab = st.tabs(["chat", "settings"])
     # Create a list that will store the chat history, if one does not already exist
     if "messages" not in st.session_state:
@@ -32,7 +33,7 @@ def run_streamlit():
         # Present an input field where the user can type a message to Redbox
         if prompt := st.chat_input("How can I help you today?"):
             # New messages should be added to the chat history under the "user" role
-            st.session_state.messages.append({"role":"user", "text":prompt})
+            st.session_state.messages.append({"role": "user", "text": prompt})
             # Construct a Redbox object with required information
             state = RedboxState(
                 request=RedboxQuery(
@@ -40,20 +41,20 @@ def run_streamlit():
                     s3_keys=[],
                     user_uuid=uuid4(),
                     chat_history=[],
-                    ai_settings=st.session_state.ai_settings
+                    ai_settings=st.session_state.ai_settings,
                 ),
             )
-            #Run an asynchronous function from the Redbox instance to get the AI's response
+            # Run an asynchronous function from the Redbox instance to get the AI's response
             response: RedboxState = asyncio.run(st.session_state.redbox.run(state))
             llm_answer = response["text"]
-            route = response["route_name"]
+            # route = response["route_name"] - to be added back in if / when routing is added
             # Display the AI's response in the chat under the "ai" role and append it to the chat history
             with st.chat_message("ai"):
                 st.write(llm_answer)
-            st.session_state.messages.append({"role":"ai", "text":llm_answer})
-            
-            
+            st.session_state.messages.append({"role": "ai", "text": llm_answer})
+
+
 run_streamlit()
 
-# getting the chat history to work, showing the routes, and sources, and getting it to look like Redbox. 
+# getting the chat history to work, showing the routes, and sources, and getting it to look like Redbox.
 # Go back to James re-files, data and streaming
