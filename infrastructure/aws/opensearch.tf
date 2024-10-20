f58dfd8daa22f1d84c960f6ca9efa8e321f02c4f
gecBurton <g.e.c.burton@gmail.com>
2024-10-20 15:20:40 +0100

    added oss module

--- infrastructure/aws/opensearch.tf
+++ infrastructure/aws/opensearch.tf
@@ -1,10 +1,12 @@
+
 module "opensearch" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
-  source          = "git@github.com:i-dot-ai/i-ai-core-infrastructure.git//modules/opensearch"
-  environment     = var.env
-  state_bucket    = var.state_bucket
-  collection_name = "redbox-documents"
-  region          = var.region
-  type            = "VECTORSEARCH"
-  account_id      = var.account_id
+  # source = "../../../i-dot-ai-core-terraform-modules//modules/infrastructure/rds"  # For testing local changes
+  source                  = "git@github.com:i-dot-ai/i-ai-core-infrastructure.git///modules/opensearch"
+  account_id = var.account_id
+  collection_name = "${var.team_name}-${terraform.workspace}-${var.project_name}-collection"
+  type = "SEARCH"
+  environment = var.env
+  region = var.region
+  state_bucket = var.state_bucket
 }
