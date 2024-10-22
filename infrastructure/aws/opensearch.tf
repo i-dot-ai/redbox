b546554de03f970f5fd57f5b10676373c26d0f0f
gecBurton <g.e.c.burton@gmail.com>
2024-10-22 10:59:13 +0100

    updated opensearch

--- infrastructure/aws/opensearch.tf
+++ infrastructure/aws/opensearch.tf
@@ -1,12 +1,11 @@
-
 module "opensearch" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
-  # source = "../../../i-dot-ai-core-terraform-modules//modules/infrastructure/rds"  # For testing local changes
-  source                  = "git@github.com:i-dot-ai/i-ai-core-infrastructure.git///modules/opensearch"
-  account_id = var.account_id
+  # source = "../../../i-ai-core-infrastructure//modules/opensearch"  # For testing local changes
+  source          = "git::https://github.com/i-dot-ai/i-ai-core-infrastructure.git//modules/opensearch"
+  account_id      = var.account_id
   collection_name = "${var.team_name}-${terraform.workspace}-${var.project_name}-collection"
-  type = "SEARCH"
-  environment = var.env
-  region = var.region
-  state_bucket = var.state_bucket
+  type            = "SEARCH"
+  environment     = var.env
+  region          = var.region
+  state_bucket    = var.state_bucket
 }
