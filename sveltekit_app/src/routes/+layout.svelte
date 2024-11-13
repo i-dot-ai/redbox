46e5a4d26cc12f48f6c07291db268139572e2abe
Kevin Etchells <kevetchells@hotmail.com>
2024-11-13 12:30:13 +0000

    Build demo homepage in Sveltekit
    Add missing files from previous commit

---
+++ sveltekit_app/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Footer from '$lib/components/footer.svelte';
+	import Header from '$lib/components/top-nav.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot></slot>
+</main>
+
+<Footer />
