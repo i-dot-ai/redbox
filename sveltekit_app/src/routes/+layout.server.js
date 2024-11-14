5d76154aa0fb58f023811070fc5e1a003d38df50
Kevin Etchells <kevetchells@hotmail.com>
2024-11-14 12:16:22 +0000

    Pull in user data from Django API
    Fix previous commit

--- sveltekit_app/src/routes/+layout.server.js
+++ sveltekit_app/src/routes/+layout.server.js
@@ -1,5 +1,29 @@
-export function load({ request, cookies }) {
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ request, cookies }) {
+
+  const API_URL = "http://localhost:8091/api/v0?format=json";
+
+  const userRequest = await fetch(API_URL, {
+    headers: {
+      cookie: `sessionid=${cookies.get("sessionid")}`,
+    },
+  });
+  const userData = userRequest.ok ? await userRequest.json() : {};
+
+  const publicRoutes = [
+    "/",
+    "/sign-in",
+    "/privacy",
+    "/accessibility",
+    "support",
+    "sitemap",
+  ];
+  // TO DO: Add a redirect here if user is not authenticated, unless one of the publicRoutes
+
   return {
-    userIsAuthenticated: cookies.get("sessionid") ? true : false,
+    userIsAuthenticated: userData.email ? true : false,
+    user: userData,
   };
+  
 }
