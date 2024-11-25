d2dc4772a65315c874bde49209494f56c010b82c
Kevin Etchells <kevetchells@hotmail.com>
2024-11-25 08:25:40 +0000

    Add demo Lit component

---
+++ django_app/frontend/src/js/web-components/chats/activity-button-lit.js
@@ -0,0 +1,30 @@
+// @ts-check
+import { LitElement, html } from "lit";
+
+class ActivityButton extends LitElement {
+  static properties = {
+    expanded: { type: Boolean, reflect: true },
+  };
+
+  createRenderRoot() {
+    return this;
+  }
+
+  constructor() {
+    super();
+    this.expanded = false;
+  }
+
+  render() {
+    return html`
+      <button @click=${this.#buttonClick} type="button">
+        ${this.expanded ? `- Hide all activity` : `+ Show all activity`}
+      </button>
+    `;
+  }
+
+  #buttonClick = () => {
+    this.expanded = !this.expanded;
+  };
+}
+customElements.define("activity-button", ActivityButton);
