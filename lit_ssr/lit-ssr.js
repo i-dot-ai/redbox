417dc4527316f6a4045465736fc9be61c45ea6b3
Kevin Etchells <kevetchells@hotmail.com>
2024-11-29 08:58:51 +0000

    Use dynamic imports for Lit-ssr components

--- lit_ssr/lit-ssr.js
+++ lit_ssr/lit-ssr.js
@@ -1,39 +1,81 @@
-import express from 'express';
-import { html, render } from '@lit-labs/ssr';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-import DOMPurify from 'dompurify';
-import { JSDOM } from 'jsdom';
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { html, render } from "@lit-labs/ssr";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 
-// import components
-// TO DO: How can we import these automatically?
-import { ActivityButton } from '../django_app/frontend/src/js/web-components/chats/activity-button-lit.mjs';
-import { ActionButtons } from '../django_app/frontend/src/js/web-components/chats/action-buttons.mjs';
+const COMPONENTS_DIRECTORY = "../django_app/frontend/src/js/web-components";
 
-const window = new JSDOM('').window;
+let components = [];
+const window = new JSDOM("").window;
 const purify = DOMPurify(window);
-
 const app = express();
 
+// Import components
+(() => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const directoryPath = path.join(__dirname, COMPONENTS_DIRECTORY);
+
+  const capitalise = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const readFiles = (dir) => {
+    fs.readdirSync(dir).forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        // Recursively read sub-directory
+        readFiles(filePath);
+      } else if (stat.isFile()) {
+        if (!filePath.endsWith(".mjs")) {
+          return;
+        }
+
+        // Get component tag, so we can tell DomPurify to allow it
+        const componentTag = path.basename(filePath).replace(".mjs", ""); // Get the filename without the full path
+        components.push(componentTag);
+
+        // Dynamically import componentName from filePath
+        import(filePath)
+          .then((module) => {
+            console.log(`Imported <${componentTag}> from ${filePath}`);
+          })
+          .catch((error) => {
+            console.error(`Failed to import component from ${filePath}`, error);
+          });
+      }
+    });
+  };
+
+  readFiles(directoryPath);
+})();
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   //console.log("Input", req.query.data);
   const renderedHtml = [...render(html`${unsafeHTML(req.query.data)}`)]
     .join("")
-    .replace(/<template shadowroot="open" shadowrootmode="open">/g, '')
-    .replace(/<\/template>/g, '');
+    .replace(/<template shadowroot="open" shadowrootmode="open">/g, "")
+    .replace(/<\/template>/g, "");
 
   const sanitisedHtml = purify.sanitize(renderedHtml, {
-    ADD_TAGS: ['activity-button', 'action-buttons'],
+    ADD_TAGS: components,
   });
-  
+
   //console.log("Output", sanitisedHtml);
   res.send(sanitisedHtml);
 });
 
 app.listen(3002, () => {
-  console.log('Server is running on port 3002');
+  console.log("Server is running on port 3002");
 });
