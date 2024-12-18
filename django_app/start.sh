d4878962a371547c303700d328d1c5086df4af2f
nora-errouhly <nora.er-rouhly@digital.trade.gov.uk>
2024-12-18 12:31:52 +0000

    fix port number issue

--- django_app/start.sh
+++ django_app/start.sh
@@ -4,4 +4,4 @@ venv/bin/django-admin migrate
 venv/bin/django-admin collectstatic --noinput
 venv/bin/django-admin create_admin_user
 
-venv/bin/daphne -b 0.0.0.0 -p 8080 redbox_app.asgi:application
+venv/bin/daphne -b 0.0.0.0 -p 8090 redbox_app.asgi:application
