93709db90bf4c036c4debcf972649b69bad409f0
gecBurton <g.e.c.burton@gmail.com>
2024-12-19 14:31:10 +0000

    metadata removed

--- redbox-core/tests/test_ingest.py
+++ redbox-core/tests/test_ingest.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 from unittest.mock import MagicMock, patch
@@ -8,15 +7,12 @@ from _pytest.monkeypatch import MonkeyPatch
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan
 from langchain_core.embeddings.fake import FakeEmbeddings
-from langchain_core.language_models.fake_chat_models import GenericFakeChatModel
 from langchain_elasticsearch import ElasticsearchStore
 
 
-from redbox.models.chain import GeneratedMetadata
 from redbox.chains.ingest import document_loader, ingest_from_loader
 from redbox.loader import ingester
 from redbox.loader.loaders import (
-    MetadataLoader,
     UnstructuredChunkLoader,
 )
 from redbox.models.file import ChunkResolution
@@ -55,82 +51,9 @@ def make_file_query(file_name: str, resolution: ChunkResolution | None = None) -
     return {"query": {"bool": {"must": [{"match_all": {}}], "filter": query_filter}}}
 
 
-def fake_llm_response():
-    return {
-        "name": "example.html",
-        "description": None,
-        "keywords": [],
-    }
-
-
-@patch("redbox.loader.loaders.get_chat_llm")
-def test_extract_metadata_missing_key(
-    mock_llm: MagicMock,
-    env: Settings,
-    s3_client: S3Client,
-    requests_mock,
-):
-    mock_llm_response = mock_llm.return_value
-    mock_llm_response.status_code = 200
-    mock_llm_response.return_value = GenericFakeChatModel(messages=iter(['{"missing_key":""}']))
-
-    requests_mock.post(
-        f"http://{env.unstructured_host}:8000/general/v0/general",
-        json=[{"text": "hello", "metadata": {}}],
-    )
-
-    """
-    LLM replies but without one of the keys
-    """
-
-    # Upload file
-    file_name = file_to_s3("html/example.html", s3_client, env)
-
-    metadata_loader = MetadataLoader(env=env, s3_client=s3_client, file_name=file_name)
-    metadata = metadata_loader.extract_metadata()
-
-    assert metadata == GeneratedMetadata()
-
-
-@patch("redbox.loader.loaders.get_chat_llm")
-def test_extract_metadata_extra_key(
-    mock_llm: MagicMock,
-    env: Settings,
-    s3_client: S3Client,
-    requests_mock,
-):
-    mock_llm_response = mock_llm.return_value
-    mock_llm_response.status_code = 200
-    mock_llm_response.return_value = GenericFakeChatModel(
-        messages=iter(['{"extra_key": "", "name": "foo", "description": "test", "keywords": ["abc"]}'])
-    )
-
-    requests_mock.post(
-        f"http://{env.unstructured_host}:8000/general/v0/general",
-        json=[{"text": "hello", "metadata": {}}],
-    )
-
-    """
-    LLM replies with an extra key
-    """
-
-    # Upload file
-    file_name = file_to_s3("html/example.html", s3_client, env)
-
-    metadata_loader = MetadataLoader(env=env, s3_client=s3_client, file_name=file_name)
-    metadata = metadata_loader.extract_metadata()
-
-    assert metadata is not None
-    assert metadata.name == "foo"
-    assert metadata.description == "test"
-    assert metadata.keywords == ["abc"]
-
-
-@patch("redbox.loader.loaders.get_chat_llm")
 @patch("redbox.loader.loaders.requests.post")
 def test_document_loader(
     mock_post: MagicMock,
-    mock_llm: MagicMock,
     s3_client: S3Client,
     env: Settings,
 ):
@@ -156,22 +79,14 @@ def test_document_loader(
         }
     ]
 
-    mock_llm_response = mock_llm.return_value
-    mock_llm_response.status_code = 200
-    mock_llm_response.return_value = GenericFakeChatModel(messages=iter([json.dumps(fake_llm_response())]))
-
     # Upload file
     file = file_to_s3("html/example.html", s3_client, env)
-
-    metadata_loader = MetadataLoader(env=env, s3_client=s3_client, file_name=file)
-    metadata = metadata_loader.extract_metadata()
 
     loader = UnstructuredChunkLoader(
         chunk_resolution=ChunkResolution.normal,
         env=env,
         min_chunk_size=env.worker_ingest_min_chunk_size,
         max_chunk_size=env.worker_ingest_max_chunk_size,
-        metadata=metadata,
     )
 
     # Call loader
@@ -180,15 +95,7 @@ def test_document_loader(
 
     assert len(chunks) > 0
 
-    # Verify that metadata has been attached to object
-    for chuck in chunks:
-        llm_response = fake_llm_response()
-        assert chuck.metadata["name"] == llm_response["name"]
-        assert chuck.metadata["description"] == llm_response["description"]
-        assert chuck.metadata["keywords"] == llm_response["keywords"]
 
-
-@patch("redbox.loader.loaders.get_chat_llm")
 @patch("redbox.loader.loaders.requests.post")
 @pytest.mark.parametrize(
     "resolution, has_embeddings",
@@ -199,7 +106,6 @@ def test_document_loader(
 )
 def test_ingest_from_loader(
     mock_post: MagicMock,
-    mock_llm: MagicMock,
     resolution: ChunkResolution,
     has_embeddings: bool,
     monkeypatch: MonkeyPatch,
@@ -232,23 +138,14 @@ def test_ingest_from_loader(
         }
     ]
 
-    mock_llm_response = mock_llm.return_value
-    mock_llm_response.status_code = 200
-    mock_llm_response.return_value = GenericFakeChatModel(messages=iter([json.dumps(fake_llm_response())]))
-
     # Upload file and call
     file_name = file_to_s3(filename="html/example.html", s3_client=s3_client, env=env)
-
-    # Extract metadata
-    metadata_loader = MetadataLoader(env=env, s3_client=s3_client, file_name=file_name)
-    metadata = metadata_loader.extract_metadata()
 
     loader = UnstructuredChunkLoader(
         chunk_resolution=resolution,
         env=env,
         min_chunk_size=env.worker_ingest_min_chunk_size,
         max_chunk_size=env.worker_ingest_max_chunk_size,
-        metadata=metadata,
     )
 
     # Mock embeddings
@@ -267,14 +164,6 @@ def test_ingest_from_loader(
     def get_metadata(chunk: dict) -> dict:
         return chunk["_source"]["metadata"]
 
-    # Verify that metadata has been attached to object
-    if has_embeddings:
-        for chunk in chunks:
-            metadata = get_metadata(chunk)
-            assert metadata["name"] == fake_llm_response()["name"]
-            assert metadata["description"] == fake_llm_response()["description"]
-            assert metadata["keywords"] == fake_llm_response()["keywords"]
-
     if has_embeddings:
         embeddings = chunks[0]["_source"].get(env.embedding_document_field_name)
         assert embeddings is not None
@@ -284,7 +173,6 @@ def test_ingest_from_loader(
     es_client.delete_by_query(index=es_index, body=file_query)
 
 
-@patch("redbox.loader.loaders.get_chat_llm")
 @patch("redbox.loader.loaders.requests.post")
 @pytest.mark.parametrize(
     ("filename", "is_complete", "mock_json"),
@@ -311,7 +199,6 @@ def test_ingest_from_loader(
 )
 def test_ingest_file(
     mock_post: MagicMock,
-    mock_llm: MagicMock,
     es_client: Elasticsearch,
     s3_client: S3Client,
     monkeypatch: MonkeyPatch,
@@ -339,11 +226,6 @@ def test_ingest_file(
     # Upload file and call
     filename = file_to_s3(filename=filename, s3_client=s3_client, env=env)
 
-    # Mock llm
-    mock_llm_response = mock_llm.return_value
-    mock_llm_response.status_code = 200
-    mock_llm_response.return_value = GenericFakeChatModel(messages=iter([json.dumps(fake_llm_response())]))
-
     res = ingest_file(filename)
 
     if not is_complete:
@@ -356,17 +238,6 @@ def test_ingest_file(
 
         chunks = list(scan(client=es_client, index=f"{es_index}-current", query=file_query))
         assert len(chunks) > 0
-
-        def get_metadata(chunk: dict) -> dict:
-            return chunk["_source"]["metadata"]
-
-        # Verify that metadata has been attached to document.
-        for chunk in chunks:
-            metadata = get_metadata(chunk)
-            llm_response = fake_llm_response()
-            assert metadata["name"] == llm_response["name"]
-            assert metadata["description"] == llm_response["description"]
-            assert metadata["keywords"] == llm_response["keywords"]
 
         def get_chunk_resolution(chunk: dict) -> str:
             return chunk["_source"]["metadata"]["chunk_resolution"]
