a052105c6ba225db7bf47d4cca0ee1029dbc7059
Kevin Etchells <kevetchells@hotmail.com>
2024-12-19 14:45:17 +0000

    Get doc IDs from new endpoint

--- django_app/frontend/src/js/web-components/documents/file-status.js
+++ django_app/frontend/src/js/web-components/documents/file-status.js
@@ -1,34 +1,47 @@
 // @ts-check
 
 class FileStatus extends HTMLElement {
+
+  static observedAttributes = ["data-id"];
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "data-id" && oldValue !== newValue) {
+      this.#checkStatus();
+    }
+  }
+
   connectedCallback() {
-    const checkStatus = async () => {
-      // UPDATE THESE AS REQUIRED
-      const FILE_STATUS_ENDPOINT = "/file-status";
-      const CHECK_INTERVAL_MS = 6000;
+    this.#checkStatus();
+  }
 
-      const response = await fetch(
-        `${FILE_STATUS_ENDPOINT}?id=${this.dataset.id}`
-      );
-      const responseObj = await response.json();
-      this.textContent = responseObj.status;
-      this.dataset.status = responseObj.status.toLowerCase();
+  async #checkStatus () {
 
-      if (responseObj.status.toLowerCase() === "complete") {
-        const evt = new CustomEvent("doc-complete", {
-          detail: this,
-        });
-        document.body.dispatchEvent(evt);
-      } else {
-        window.setTimeout(checkStatus, CHECK_INTERVAL_MS);
-      }
-    };
+    // UPDATE THESE AS REQUIRED
+    const FILE_STATUS_ENDPOINT = "/file-status";
+    const CHECK_INTERVAL_MS = 6000;
 
-    if (this.textContent.toLowerCase() !== "complete") {
-      checkStatus();
+    if (this.textContent?.toLowerCase() === "complete" || !this.dataset.id) {
+      return;
     }
 
-  }
+    const response = await fetch(
+      `${FILE_STATUS_ENDPOINT}?id=${this.dataset.id}`
+    );
+    const responseObj = await response.json();
+    this.textContent = responseObj.status;
+    this.dataset.status = responseObj.status.toLowerCase();
+
+    if (responseObj.status.toLowerCase() === "complete") {
+      const evt = new CustomEvent("doc-complete", {
+        detail: this,
+      });
+      document.body.dispatchEvent(evt);
+    } else {
+      window.setTimeout(() => {
+        this.#checkStatus();
+      }, CHECK_INTERVAL_MS);
+    }
+
+  };
 
 }
 customElements.define("file-status", FileStatus);
