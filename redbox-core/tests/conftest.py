231b531be82fd8949965f25a71070ffd0e6b80ee
gecBurton <g.e.c.burton@gmail.com>
2024-12-30 14:32:49 +0000

    mocking out new arg names

--- redbox-core/tests/conftest.py
+++ redbox-core/tests/conftest.py
@@ -94,7 +94,7 @@ class FileManager:
     def __init__(self, docs):
         self.docs = docs
 
-    def filter(self, original_file__in):
+    def filter(self, original_file__in, text__is_null, metadata__is_null):
         for doc in self.docs:
             yield FakeFile(doc.page_content, doc.metadata)
 
