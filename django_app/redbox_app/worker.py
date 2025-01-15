00eb928bb6f591581c0ba6ae76e953edbd9a471f
gecBurton <g.e.c.burton@gmail.com>
2025-01-15 17:38:11 +0000

    added uri

--- django_app/redbox_app/worker.py
+++ django_app/redbox_app/worker.py
@@ -21,7 +21,7 @@ def ingest(file_id: UUID) -> None:
     try:
         markdown = md.convert(file.url)
         file.text = sanitise_string(markdown.text_content)
-        file.metadata = {"token_count": len(tokeniser.encode(markdown.text_content))}
+        file.metadata = {"token_count": len(tokeniser.encode(markdown.text_content)), "uri": file.url}
         file.status = File.Status.complete
         file.save()
     except Exception as error:  # noqa: BLE001
