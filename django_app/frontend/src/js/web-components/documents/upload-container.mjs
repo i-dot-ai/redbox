8318969f28ae9030e0b958e9a01ff45046750036
Kevin Etchells <kevetchells@hotmail.com>
2025-01-24 14:29:12 +0000

    Move focus when file is deleted

--- django_app/frontend/src/js/web-components/documents/upload-container.mjs
+++ django_app/frontend/src/js/web-components/documents/upload-container.mjs
@@ -11,6 +11,7 @@ export class UploadContainer extends RedboxElement {
     super.connectedCallback();
     // on message-send move the uploaded docs to the correct container
     document.addEventListener("chat-response-start", () => {
+      /** @type {NodeListOf<import("./document-container.mjs").DocumentContainer>} */
       const documentContainers = document.querySelectorAll("document-container");
       const lastDocumentContainer = documentContainers[documentContainers.length - 1];
       lastDocumentContainer.addDocuments(this.docs);
@@ -23,7 +24,7 @@ export class UploadContainer extends RedboxElement {
       <div class="rb-upload-container">
         <h3 class="govuk-visually-hidden">Uploading documents</h3>
         <ul>
-          ${this.docs.map(
+          ${this.docs?.map(
             (doc) => html`
               <li class="rb-upload-container__doc">
                 <file-status data-id=${doc.id} data-name=${doc.file_name}>${doc.file_status}</file-status>
@@ -43,7 +44,7 @@ export class UploadContainer extends RedboxElement {
   
     const tempId = crypto.randomUUID();
 
-    this.docs.push({
+    this.docs?.push({
       temp_id: tempId,
       file_name: doc.name,
       status: "Uploading"
@@ -52,12 +53,12 @@ export class UploadContainer extends RedboxElement {
 
     const formData = new FormData();
     formData.append('file', doc);
-    formData.append('chat_id', this.dataset.chatid);
+    formData.append('chat_id', this.dataset.chatid || "");
 
     const response = await fetch("/api/v0/file/", {
       method: "POST",
       headers: {
-        "X-CSRFToken": this.dataset.csrftoken,
+        "X-CSRFToken": this.dataset.csrftoken || "",
       },
       body: formData,
     });
@@ -66,7 +67,9 @@ export class UploadContainer extends RedboxElement {
     }
 
     const data = await response.json();
-    this.docs.find(doc => doc.temp_id === tempId).id = data.file_id;
+    if (this.docs) {
+      this.docs.find(doc => doc.temp_id === tempId).id = data.file_id;
+    }
     this.requestUpdate();
 
   }
@@ -84,10 +87,14 @@ export class UploadContainer extends RedboxElement {
     fetch(`/remove-doc/${id}`, {
       method: "POST",
       headers: {
-        "X-CSRFToken": this.dataset.csrftoken,
+        "X-CSRFToken": this.dataset.csrftoken || "",
       },
       body: formData,
     });
+
+    window.setTimeout(() => {
+      /** @type {HTMLInputElement | null} */(document.querySelector("message-input textarea"))?.focus();
+    }, 100);
   }
 
 }
