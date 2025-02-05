630577e59ca8b0f75935297077f0f5e9ae252399
gecBurton <g.e.c.burton@gmail.com>
2025-02-05 12:00:01 +0000

    removed llm_conversation

--- django_app/redbox_app/redbox_core/consumers.py
+++ django_app/redbox_app/redbox_core/consumers.py
@@ -1,7 +1,6 @@
 import json
 import logging
-from asyncio import CancelledError
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping
 from typing import Any
 
 from asgiref.sync import sync_to_async
@@ -9,19 +8,15 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from openai import RateLimitError
-from websockets import ConnectionClosedError
 
 from redbox import Redbox
 from redbox_app.redbox_core import error_messages
 from redbox_app.redbox_core.models import (
-    Chat,
     ChatMessage,
-    File,
     get_chat_session,
 )
 
 User = get_user_model()
-OptFileSeq = Sequence[File] | None
 logger = logging.getLogger(__name__)
 logger.info("WEBSOCKET_SCHEME is: %s", settings.WEBSOCKET_SCHEME)
 
@@ -50,13 +45,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.close()
             return
 
-        await self.llm_conversation(chat)
-        await self.close()
-
-    async def llm_conversation(self, session: Chat) -> None:
-        """Initiate & close websocket conversation with the core-api message endpoint."""
-
-        state = await sync_to_async(session.to_langchain)()
+        state = await sync_to_async(chat.to_langchain)()
 
         try:
             state = await self.redbox.run(
@@ -65,28 +54,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
 
             message = await ChatMessage.objects.acreate(
-                chat=session,
+                chat=chat,
                 text=state.messages[-1].content,
                 role=ChatMessage.Role.ai,
             )
 
-            await self.send_to_client(
-                "end", {"message_id": message.id, "title": session.name, "session_id": session.id}
-            )
+            await self.send_to_client("end", {"message_id": message.id, "title": chat.name, "session_id": chat.id})
 
         except RateLimitError as e:
             logger.exception("Rate limit error", exc_info=e)
             await self.send_to_client("error", error_messages.RATE_LIMITED)
-        except (TimeoutError, ConnectionClosedError, CancelledError) as e:
-            logger.exception("Error from core.", exc_info=e)
-            await self.send_to_client("error", error_messages.CORE_ERROR_MESSAGE)
-        except Exception as e:
+
+        except BaseException as e:
             logger.exception("General error.", exc_info=e)
             await self.send_to_client("error", error_messages.CORE_ERROR_MESSAGE)
 
+        await self.close()
+
     async def send_to_client(self, message_type: str, data: str | Mapping[str, Any] | None = None) -> None:
         message = {"type": message_type, "data": data}
-        logger.debug("sending %s to browser", message)
         await self.send(json.dumps(message, default=str))
 
     async def handle_text(self, response: str) -> str:
