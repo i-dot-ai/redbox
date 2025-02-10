8127ec44df6d74cc8533b56bd51e6e4fc173956d
gecBurton <g.e.c.burton@gmail.com>
2025-02-10 15:50:57 +0000

    fixed test

--- django_app/tests/views/test_chat_message.py
+++ django_app/tests/views/test_chat_message.py
@@ -3,8 +3,7 @@ from unittest.mock import patch
 import pytest
 from django.urls import reverse
 from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import AIMessageChunk
-from pydantic import BaseModel
+from langchain_core.messages import AIMessage, BaseMessage
 
 from redbox_app.redbox_core.models import Chat
 
@@ -13,34 +12,18 @@ class CannedGraphLLM(BaseChatModel):
     responses: list[dict]
 
     def _generate(self, *_args, **_kwargs):
-        for _ in self.responses:
-            yield
+        yield from self.responses
 
     def _llm_type(self):
         return "canned"
 
-    async def astream_events(self, *_args, **_kwargs):
-        for response in self.responses:
-            yield response
-
-
-class Token(BaseModel):
-    content: str
+    def invoke(self, *_args, **_kwargs) -> BaseMessage:
+        return AIMessage(content=self.responses[0]["content"])
 
 
 @pytest.fixture()
 def mocked_connect():
-    responses = [
-        {
-            "event": "on_chat_model_stream",
-            "data": {"chunk": Token(content="Good afternoon, ")},
-        },
-        {"event": "on_chat_model_stream", "data": {"chunk": Token(content="Mr. Amor.")}},
-        {
-            "event": "on_chain_end",
-            "data": {"output": AIMessageChunk(content="Good afternoon, Mr. Amor.")},
-        },
-    ]
+    responses = [{"content": "no", "type": "ai"}]
 
     return CannedGraphLLM(responses=responses)
 
@@ -54,6 +37,7 @@ def test_post_pass(chat: Chat, mocked_connect, client):
     with patch(
         "redbox_app.redbox_core.views.api_views.ChatMessageView.redbox._get_runnable", new=lambda _: mocked_connect
     ):
+        message = "write me poem"
         response = client.post(url, {"message": "write me poem"})
         assert response.status_code == 200
-        assert response.json() == {}
+        assert response.json()["title"] == message
