ea94a3755f0398c3250ecf630589acd2169ccae6
George Burton <8233643+gecBurton@users.noreply.github.com>
2025-02-10 13:35:56 +0000

    Feature/move ratings endpoint to drf (#1374)
    * feature/move-ratings-endpoint-to-drf
    
    * remove files added in error"
    
    * feature/move-ratings-endpoint-to-drf
    
    * sanitize json
    
    * bug fix
    
    * bug fix
    
    * bug fix

--- django_app/redbox_app/redbox_core/views/api_views.py
+++ django_app/redbox_app/redbox_core/views/api_views.py
@@ -1,13 +1,20 @@
+import logging
+from http import HTTPStatus
+from uuid import UUID
+
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.fields import FileField, UUIDField
+from rest_framework.fields import CharField, FileField, IntegerField, ListField, UUIDField
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 
-from redbox_app.redbox_core.models import File
+from redbox_app.redbox_core.models import ChatMessage, File
+from redbox_app.redbox_core.utils import sanitize_json
 
 User = get_user_model()
+logger = logging.getLogger(__name__)
 
 
 class UploadSerializer(Serializer):
@@ -32,3 +39,33 @@ def file_upload(request):
     )
     file.ingest()
     return Response({"file_id": file.id}, status=200)
+
+
+class RatingSerializer(Serializer):
+    rating = IntegerField()
+    text = CharField(required=False)
+    chips = ListField(child=CharField(), required=False)
+
+    def to_internal_value(self, data):
+        data = sanitize_json(data)
+        return super().to_internal_value(data)
+
+    class Meta:
+        fields = "__all__"
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def rate_chat_message(request, message_id: UUID):
+    serializer = RatingSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    logger.info("getting chat-message with id=%s", message_id)
+
+    message: ChatMessage = get_object_or_404(ChatMessage, id=message_id)
+
+    message.rating = serializer.validated_data["rating"]
+    message.rating_text = serializer.validated_data.get("text")
+    message.rating_chips = sorted(serializer.validated_data.get("chips", []))
+    message.save()
+    return Response({}, status=HTTPStatus.OK)
