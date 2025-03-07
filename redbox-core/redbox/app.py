ca5b9233b28c5932d9f49ab1c54833247683c201
gecBurton <g.e.c.burton@gmail.com>
2025-03-07 10:07:01 +0000

    feature/simplify-redbox-core

--- redbox-core/redbox/app.py
+++ redbox-core/redbox/app.py
@@ -1,7 +1,8 @@
 from logging import getLogger
 
 from langchain.chat_models import init_chat_model
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.messages import BaseMessage
+from langchain_core.prompts import PromptTemplate
 
 from redbox.models.chain import RedboxState
 from redbox.models.settings import get_settings
@@ -19,36 +20,34 @@ class Redbox:
         self.debug = debug
 
     def _get_runnable(self, state: RedboxState):
-        settings = get_settings()
         if state.chat_backend.provider == "google_vertexai":
-            llm = init_chat_model(
+            return init_chat_model(
                 model=state.chat_backend.name,
                 model_provider=state.chat_backend.provider,
                 location="europe-west1",
                 # europe-west1 = Belgium
             )
-        else:
-            llm = init_chat_model(
-                model=state.chat_backend.name,
-                model_provider=state.chat_backend.provider,
-            )
+        return init_chat_model(
+            model=state.chat_backend.name,
+            model_provider=state.chat_backend.provider,
+        )
+
+    def get_messages(self, state: RedboxState) -> list[BaseMessage]:
+        settings = get_settings()
 
         input_state = state.model_dump()
-        messages = (
-            [settings.system_prompt_template]
-            + state.messages[:-1]
-            + PromptTemplate.from_template(settings.question_prompt_template, template_format="jinja2")
+        system_messages = (
+            PromptTemplate.from_template(settings.system_prompt_template, template_format="jinja2")
             .invoke(input=input_state)
             .to_messages()
         )
-        return ChatPromptTemplate.from_messages(messages=messages) | llm
+        return system_messages + state.messages
 
     def run_sync(self, state: RedboxState):
         """
         Run Redbox without streaming events. This simpler, synchronous execution enables use of the graph debug logging
         """
-        request_dict = state.model_dump()
-        return self._get_runnable(state).invoke(input=request_dict)
+        return self._get_runnable(state).invoke(input=self.get_messages(state))
 
     async def run(
         self,
@@ -56,10 +55,8 @@ class Redbox:
         response_tokens_callback=_default_callback,
     ) -> RedboxState:
         final_state = None
-        request_dict = state.model_dump()
-        logger.info("Request: %s", {k: request_dict[k] for k in request_dict.keys() - {"ai_settings"}})
         async for event in self._get_runnable(state).astream_events(
-            request_dict,
+            self.get_messages(state),
             version="v2",
         ):
             kind = event["event"]
